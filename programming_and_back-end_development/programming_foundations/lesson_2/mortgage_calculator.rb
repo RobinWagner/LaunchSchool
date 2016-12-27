66998ac703563f6b8ed1208ddaa50a96386a2682
Robin Wagner <robin-wagner@web.de>
2016-12-27 20:49:44 +0100

    Add input validation

--- programming_and_back-end_development/programming_foundations/lesson_2/mortgage_calculator.rb
+++ programming_and_back-end_development/programming_foundations/lesson_2/mortgage_calculator.rb
@@ -1,14 +1,61 @@
 # Mortgage / Car Loan Calculator
 
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def number?(input)
+  integer?(input) || float?(input)
+end
+
+def integer?(input)
+  /^\d+$/.match(input)
+end
+
+def float?(input)
+  /\d/.match(input) && /^\d*\.?\d*$/.match(input)
+end
+
 puts "Welcome to the mortgage calculator."
-puts "Please enter the loan amount you would like to borrow: "
-loan_amount = gets.chomp.to_f
 
-puts "Please enter the Annual Percentage Rate (APR) (in %): "
-annual_percentage_rate = gets.chomp.to_f / 100
+loan_amount = ''
+loop do
+  prompt("Please enter the loan amount you would like to borrow: ")
+  loan_amount = gets.chomp
 
-puts "Please enter the loan duration (in years): "
-loan_duration_month = gets.chomp.to_f * 12
+  if number?(loan_amount)
+    loan_amount = loan_amount.to_f
+    break
+  else
+    prompt("Invalid input.")
+  end
+end
+
+annual_percentage_rate = ''
+loop do
+  prompt("Please enter the Annual Percentage Rate (APR) (in %): ")
+  annual_percentage_rate = gets.chomp
+
+  if number?(annual_percentage_rate)
+    annual_percentage_rate = annual_percentage_rate.to_f / 100
+    break
+  else
+    prompt("Invalid input.")
+  end
+end
+
+loan_duration_month = ''
+loop do
+  prompt("Please enter the loan duration (in years): ")
+  loan_duration_month = gets.chomp
+
+  if number?(loan_duration_month)
+    loan_duration_month = loan_duration_month.to_f * 12
+    break
+  else
+    prompt("Invalid input.")
+  end
+end
 
 monthly_interest_rate = annual_percentage_rate / 12
 
