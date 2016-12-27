15a9d8740b3aa1d5d9750b7083d85e0ee5fc1f9e
Robin Wagner <robin-wagner@web.de>
2016-12-27 19:29:16 +0100

    First version mortgage calculator

---
+++ programming_and_back-end_development/programming_foundations/lesson_2/mortgage_calculator.rb
@@ -0,0 +1,18 @@
+# Mortgage / Car Loan Calculator
+
+puts "Welcome to the mortgage calculator."
+puts "Please enter the loan amount you would like to borrow: "
+loan_amount = gets.chomp.to_f
+
+puts "Please enter the Annual Percentage Rate (APR) (in %): "
+annual_percentage_rate = gets.chomp.to_f / 100
+
+puts "Please enter the loan duration (in years): "
+loan_duration_month = gets.chomp.to_f * 12
+
+monthly_interest_rate = annual_percentage_rate / 12
+
+# Calculate monthly payment
+monthly_payment = loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-loan_duration_month)))
+
+puts "The monthly payment is $ #{monthly_payment.round(2)} a month."
