46421989150eef3f9d0f82093ffa0e60e01214e0
Robin Wagner <robin-wagner@web.de>
2016-12-31 18:38:50 +0100

    Add scores for players

--- programming_and_back-end_development/programming_foundations/lesson_2/rock_paper_scissors/rock_paper_scissors.rb
+++ programming_and_back-end_development/programming_foundations/lesson_2/rock_paper_scissors/rock_paper_scissors.rb
@@ -3,6 +3,8 @@
 VALID_CHOICES = { 'r' => 'rock', 'p' => 'paper', 'sc' => 'scissors',
                   'l' => 'lizard', 'sp' => 'spock' }
 
+scores = [0, 0]
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
@@ -25,26 +27,51 @@ def display_results(player, computer)
   end
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: ")
-    VALID_CHOICES.each { |abb, name| prompt "'#{abb}' for '#{name}'" }
-    choice = Kernel.gets().chomp()
-
-    if VALID_CHOICES.key?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
+def calculate_score(player, computer, score)
+  if win?(player, computer)
+    score[0] += 1
+  elsif win?(computer, player)
+    score[1] += 1
   end
+  score
+end
 
-  comp_choice = VALID_CHOICES.values.sample()
+def display_winner_game(scores)
+  if scores[0] > scores[1]
+    prompt("You won #{scores[0]} to #{scores[1]}! Congratulations!")
+  else
+    prompt("You lost #{scores[0]} to #{scores[1]}. Give it another try!")
+  end
+end
 
-  prompt("You chose: #{VALID_CHOICES[choice]}; Computer chose: #{comp_choice}")
+loop do
+  loop do
+    choice = ''
+    loop do
+      prompt("Choose one: ")
+      VALID_CHOICES.each { |abb, name| prompt "'#{abb}' for '#{name}'" }
+      choice = Kernel.gets().chomp()
 
-  display_results(VALID_CHOICES[choice], comp_choice)
+      if VALID_CHOICES.key?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
+    end
 
+    comp_choice = VALID_CHOICES.values.sample()
+
+    prompt("You chose: #{VALID_CHOICES[choice]}; Computer chose: #{comp_choice}")
+
+    scores = calculate_score(VALID_CHOICES[choice], comp_choice, scores)
+    display_results(VALID_CHOICES[choice], comp_choice)
+    prompt("Your score: #{scores[0]}")
+    prompt("Computer score: #{scores[1]}")
+    prompt("---")
+    break if scores.max > 4
+  end
+  display_winner_game(scores)
+  prompt("------------------------------")
   prompt("Do you want to play again? (y)")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
