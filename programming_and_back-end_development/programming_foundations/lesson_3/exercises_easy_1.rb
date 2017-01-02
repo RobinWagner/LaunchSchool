0b78b529a734249af6c64c1edaed22f37eac35d5
Robin Wagner <robin-wagner@web.de>
2017-01-02 16:36:25 +0100

    Completed exercise 2

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_1.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_1.rb
@@ -5,3 +5,17 @@
 # => 2
 # => 3
 # Prints out unchanged array since uniq method is not destructive (unlike uniq!)
+
+# Question 2
+
+# ! ("bang") after a method name is frequently used to showcase a destructive method.
+# ! together with a Boolean expression means "not"
+# ? after a method name is conventionally used to signify Boolean methods
+# ? is also used in the ternary operator
+
+# != means not equal is used in boolean expressions
+# ! before something means 'not'. It reverses the Boolean value of an expression (e.g. !true == false)
+# ! after a method is used to show a destructive method which mutates the caller (convention)
+# ? before something is used in a ternary expression to show the 'if' branch
+# ? after something is used to show a Boolean method
+# !! before something is used to display the Boolean value of the expression
