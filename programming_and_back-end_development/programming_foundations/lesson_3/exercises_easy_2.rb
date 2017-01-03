17a75ab46a86d2efff6c644afc64c7176a3343b6
Robin Wagner <robin-wagner@web.de>
2017-01-03 15:29:41 +0100

    Question 2 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_2.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_2.rb
@@ -5,3 +5,15 @@ ages.key?("Spot")
 ages.has_key?("Spot")
 ages.include?("Spot")
 ages.member?("Spot")
+
+# Question 2
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+ages.values.inject(:+)
+
+# Alternative:
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+sum = 0
+ages.values.each { |age| sum += age }
