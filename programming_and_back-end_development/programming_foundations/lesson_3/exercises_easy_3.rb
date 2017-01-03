83c3320f24e5c52befe45fb6c2ca446968d25316
Robin Wagner <robin-wagner@web.de>
2017-01-03 16:45:42 +0100

    Question 2 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_3.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_3.rb
@@ -2,3 +2,11 @@
 
 flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+# Question 2
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones << "Dino"
+
+# Alternative:
+flintstones.push("Dino")
