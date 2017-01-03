016596e4acfee1333dad0b49b38e91aaeca2bf12
Robin Wagner <robin-wagner@web.de>
2017-01-03 16:19:38 +0100

    Question 6 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_2.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_2.rb
@@ -41,3 +41,8 @@ ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
 additional_ages = { "Marilyn" => 22, "Spot" => 237 }
 
 ages.merge!(additional_ages)
+
+# Question 6
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+ages.values.min
