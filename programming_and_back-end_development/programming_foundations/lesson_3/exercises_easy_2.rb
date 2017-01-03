15ac407b73c70bca3e73d393b7d3d69b17b8d10a
Robin Wagner <robin-wagner@web.de>
2017-01-03 16:15:07 +0100

    Question 4 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_2.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_2.rb
@@ -25,3 +25,12 @@ ages.keep_if { |_, age| age < 100 }
 # Alternative:
 ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
 ages.reject! { |_, age| age >= 100 }
+
+# Question 4
+
+munsters_description = "The Munsters are creepy in a good way."
+
+munsters_description.capitalize!
+munsters_description.swapcase!
+munsters_description.downcase!
+munsters_description.upcase!
