fb805ccacf2c8da826e4c020546d97768714f043
Robin Wagner <robin-wagner@web.de>
2017-01-03 16:53:48 +0100

    Question 3 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_3.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_easy_3.rb
@@ -10,3 +10,13 @@ flintstones << "Dino"
 
 # Alternative:
 flintstones.push("Dino")
+
+# Question 3
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones.push("Dino").push("Hoppy")
+
+# Alternatives:
+flintstones.concat(%w(Dino Hoppy))
+flintstones.insert(-1, "Dino", "Hoppy")
