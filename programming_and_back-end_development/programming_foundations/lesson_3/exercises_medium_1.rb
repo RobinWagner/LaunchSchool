a1070aabc611dfc30901edd85d6519cf1b2ff2ec
Robin Wagner <robin-wagner@web.de>
2017-01-04 11:35:47 +0100

    Question 2 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_1.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_1.rb
@@ -5,3 +5,25 @@
 # Alternative:
 indent = "The Flintstones Rock!".length - 1
 10.times { puts "The Flintstones Rock!".rjust(indent += 1) }
+
+# Question 2
+
+statement = "The Flintstones Rock"
+result = {}
+letters = ('A'..'Z').to_a + ('a'..'z').to_a
+letters.each do |letter|
+  letter_frequency = statement.scan(letter).count
+  result[letter] = letter_frequency if letter_frequency > 0
+end
+
+# Alternative:
+statement = "The Flintstones Rock"
+hash = {}
+statement = statement.gsub!(/\s+/, '').split('').sort
+statement.each do |element|
+  if hash[element]
+    hash[element] += 1
+  else
+    hash[element] = 1
+  end
+end
