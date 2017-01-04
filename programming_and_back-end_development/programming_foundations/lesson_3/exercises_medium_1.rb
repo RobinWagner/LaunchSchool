1e462094ca82ad82fd4431cac1496d0374b90cef
Robin Wagner <robin-wagner@web.de>
2017-01-04 14:18:27 +0100

    Question 4 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_1.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_1.rb
@@ -36,3 +36,21 @@ puts "the value of 40 + 2 is " + (40 + 2)
 # Fixes:
 puts "the value of 40 + 2 is " + (40 + 2).to_s
 puts "the value of 40 + 2 is #{40 + 2}"
+
+# Question 4
+
+numbers = [1, 2, 3, 4]
+numbers.each do |number|
+  p number
+  numbers.shift(1)
+end
+
+# => Output: 1, 3 (numbers is [3,4])
+
+numbers = [1, 2, 3, 4]
+numbers.each do |number|
+  p number
+  numbers.pop(1)
+end
+
+# => Output: 1, 2 (numbers is [1,2])
