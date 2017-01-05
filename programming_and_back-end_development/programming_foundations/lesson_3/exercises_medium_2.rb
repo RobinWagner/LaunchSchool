9e5575c8b69d1080fc8194ede7796ba94500c747
Robin Wagner <robin-wagner@web.de>
2017-01-05 12:52:35 +0100

    Question 2 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_2.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_2.rb
@@ -10,3 +10,15 @@ munsters = {
 total_male_age = 0
 munsters.each { |_, details| total_male_age += details['age'] if details['gender'] == 'male' }
 sum
+
+# Question 2
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each { |name, details| puts "#{name} is a #{details['age']} year old #{details['gender']}." }
