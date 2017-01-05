ad7b3f63444117a14d66cfc36ce625d9915b3d0d
Robin Wagner <robin-wagner@web.de>
2017-01-05 23:35:41 +0100

    Question 4 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_hard_1.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_hard_1.rb
@@ -71,3 +71,34 @@ mess_with_vars(one, two, three)
 puts "one is: #{one}"     # => one is: two
 puts "two is: #{two}"     # => two is: three
 puts "three is: #{three}" # => three is: one
+
+# Question 4
+
+def generate_UUID
+  characters = []
+  (0..9).each { |digit| characters << digit.to_s }
+  ('a'..'f').each { |digit| characters << digit }
+
+  uuid = ""
+  sections = [8, 4, 4, 4, 12]
+  sections.each_with_index do |section, index|
+    section.times { uuid += characters.sample }
+    uuid += '-' unless index >= sections.size - 1
+  end
+
+  uuid
+end
+
+# Alternative 1:
+require 'securerandom'
+
+def create_uuid
+  SecureRandom.uuid
+end
+
+# Alternative 2:
+def create_uuid
+  "#{SecureRandom.hex(4)}-#{SecureRandom.hex(2)}-#{SecureRandom.hex(2)}-#{SecureRandom.hex(2)}-#{SecureRandom.hex(6)}"
+end
+
+create_uuid
