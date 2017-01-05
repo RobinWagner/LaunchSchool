db6d510826125255b4e5c5b7e30a4d502479bfa7
Robin Wagner <robin-wagner@web.de>
2017-01-05 12:39:21 +0100

    Question 1 completed

---
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_2.rb
@@ -0,0 +1,12 @@
+# Question 1
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" }
+}
+
+total_male_age = 0
+munsters.each { |_, details| total_male_age += details['age'] if details['gender'] == 'male' }
+sum
