2037687bf39e122383a1ecf0a8692deb0e12ebac
Robin Wagner <robin-wagner@web.de>
2017-01-05 13:01:01 +0100

    Question 3 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_2.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_2.rb
@@ -22,3 +22,19 @@ munsters = {
 }
 
 munsters.each { |name, details| puts "#{name} is a #{details['age']} year old #{details['gender']}." }
+
+# Question 3
+
+def tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param += ["rutabaga"]
+
+  return a_string_param, an_array_param
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+my_string, my_array = tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
