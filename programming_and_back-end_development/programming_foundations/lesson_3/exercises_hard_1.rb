c7d85686761627b0d72a608842419a6084585e33
Robin Wagner <robin-wagner@web.de>
2017-01-05 22:59:46 +0100

    Question 2 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_hard_1.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_hard_1.rb
@@ -9,3 +9,12 @@ greeting
 # => nil
 # When initializing a local variable inside an if block, even if that
 # if block is not executed, the local variable is initialized to nil
+
+# Question 2
+
+greetings = { a: 'hi' }
+informal_greeting = greetings[:a]
+informal_greeting << ' there'
+
+puts informal_greeting  #  => "hi there"
+puts greetings # => { a: 'hi there'}
