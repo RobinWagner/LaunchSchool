069c14760931c72458f87ef279c855aec4e94dad
Robin Wagner <robin-wagner@web.de>
2017-01-05 12:25:01 +0100

    Question 9 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_1.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_1.rb
@@ -107,3 +107,35 @@ words = "the flintstones rock"
 def titelize(words)
   words.split.map { |word| word.capitalize }.join(' ')
 end
+
+# Question 9
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |member, info|
+  case info["age"]
+  when 0..17
+    info["age_group"] = "kid"
+  when 18..64
+    info["age_group"] = "adult"
+  else
+    info["age_group"] = "senior"
+  end
+end
+
+# Alternative:
+munsters.each do |member, info|
+  if info["age"].between?(0, 17)
+    info["age_group"] = "kid"
+  elsif info["age"].between?(18, 64)
+    info["age_group"] = "adult"
+  else
+    info["age_group"] = "senior"
+  end
+end
