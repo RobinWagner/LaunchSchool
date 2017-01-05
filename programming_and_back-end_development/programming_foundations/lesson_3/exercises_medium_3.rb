963dae654e348b3d00eb7d6fb1ca0db84825518f
Robin Wagner <robin-wagner@web.de>
2017-01-05 16:14:33 +0100

    Question 1 completed

---
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_3.rb
@@ -0,0 +1,26 @@
+# Question 1
+
+# a_outer is 42 with an id of: 85 before the block.
+# b_outer is forty two with an id of: 2152729580 before the block.
+# c_outer is [42] with an id of: 2152729540 before the block.
+# d_outer is 42 with an id of: 85 before the block.
+
+# a_outer id was 85 before the block and is: 85 inside the block.
+# b_outer id was 2152729580 before the block and is: 2152729580 inside the block.
+# c_outer id was 2152729540 before the block and is: 2152729540 inside the block.
+# d_outer id was 85 before the block and is: 85 inside the block.
+
+# a_outer inside after reassignment is 22 with an id of: 85 before and: 45 after.
+# b_outer inside after reassignment is thirty three with an id of: 2152729580 before and: 2152728320 after.
+# c_outer inside after reassignment is [44] with an id of: 2152729540 before and: 2152728280 after.
+# d_outer inside after reassignment is 44 with an id of: 85 before and: 89 after.
+
+# a_inner is 22 with an id of: 45 inside the block (compared to 45 for outer).
+# b_inner is thirty three with an id of: 2152728320 inside the block (compared to 2152728320 for outer).
+# c_inner is [44] with an id of: 2152728280 inside the block (compared to 2152728280 for outer).
+# d_inner is 44 with an id of: 89 inside the block (compared to 89 for outer).
+
+# a_outer is 22 with an id of: 85 BEFORE and: 45 AFTER the block.
+# b_outer is thirty three with an id of: 2152729580 BEFORE and: 2152728320 AFTER the block.
+# c_outer is [44] with an id of: 2152729540 BEFORE and: 2152728280 AFTER the block.
+# d_outer is 44 with an id of: 85 BEFORE and: 89 AFTER the block.
