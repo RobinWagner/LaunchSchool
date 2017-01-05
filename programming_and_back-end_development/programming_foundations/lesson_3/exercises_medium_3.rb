ae423547e18a4c8607bf9270e9e63ad7fda4b6ba
Robin Wagner <robin-wagner@web.de>
2017-01-05 22:32:18 +0100

    Question 2 completed

--- programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_3.rb
+++ programming_and_back-end_development/programming_foundations/lesson_3/exercises_medium_3.rb
@@ -24,3 +24,30 @@
 # b_outer is thirty three with an id of: 2152729580 BEFORE and: 2152728320 AFTER the block.
 # c_outer is [44] with an id of: 2152729540 BEFORE and: 2152728280 AFTER the block.
 # d_outer is 44 with an id of: 85 BEFORE and: 89 AFTER the block.
+
+# Question 2
+
+# a_outer is 42 with an id of: 85 before the block.
+# b_outer is forty two with an id of: 2152753560 before the block.
+# c_outer is [42] with an id of: 2152753540 before the block.
+# d_outer is 42 with an id of: 85 before the block.
+
+# a_outer id was 85 before the method and is: 85 inside the method.
+# b_outer id was 2152753560 before the method and is: 2152753560 inside the method.
+# c_outer id was 2152753540 before the method and is: 2152753540 inside the method.
+# d_outer id was 85 before the method and is: 85 inside the method.
+
+# a_outer inside after reassignment is 22 with an id of: 85 before and: 45 after.
+# b_outer inside after reassignment is thirty three with an id of: 2152753560 before and: 2152752300 after.
+# c_outer inside after reassignment is [44] with an id of: 2152753540 before and: 2152752280 after.
+# d_outer inside after reassignment is 44 with an id of: 85 before and: 89 after.
+
+# a_inner is 22 with an id of: 45 inside the method (compared to 45 for outer).
+# b_inner is thirty three with an id of: 2152752300 inside the method (compared to 2152752300 for outer).
+# c_inner is [44] with an id of: 2152752280 inside the method (compared to 2152752280 for outer).
+# d_inner is 44 with an id of: 89 inside the method (compared to 89 for outer).
+
+# a_outer is 42 with an id of: 85 BEFORE and: 85 AFTER the method call.
+# b_outer is forty two with an id of: 2152753560 BEFORE and: 2152753560 AFTER the method call.
+# c_outer is [42] with an id of: 2152753540 BEFORE and: 2152753540 AFTER the method call.
+# d_outer is 42 with an id of: 85 BEFORE and: 85 AFTER the method call.
