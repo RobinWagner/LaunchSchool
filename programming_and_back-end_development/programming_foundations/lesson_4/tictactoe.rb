05bd8e5ff44c73be2dfea0706761afb2b2f378cb
Robin Wagner <robin-wagner@web.de>
2017-01-09 06:23:07 +0100

    Refactored AI defense

--- programming_and_back-end_development/programming_foundations/lesson_4/tictactoe.rb
+++ programming_and_back-end_development/programming_foundations/lesson_4/tictactoe.rb
@@ -72,15 +72,25 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
+  square = nil
   WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(PLAYER_MARKER) == 2 && brd.values_at(*line).count(INITIAL_MARKER) == 1
-      square = line.select { |num| brd[num] == INITIAL_MARKER }
-      brd[square[0]] = COMPUTER_MARKER
-      return
-    end
+    square = find_at_risk_square(line, brd)
+    break if square
   end
-  square = empty_squares(brd).sample
+
+  if !square
+    square = empty_squares(brd).sample
+  end
+
   brd[square] = COMPUTER_MARKER
+end
+
+def find_at_risk_square(line, board)
+  if board.values_at(*line).count(PLAYER_MARKER) == 2
+    board.select { |k, v| line.include?(k) && v == ' ' }.keys.first
+  else
+    nil
+  end
 end
 
 def board_full?(brd)
