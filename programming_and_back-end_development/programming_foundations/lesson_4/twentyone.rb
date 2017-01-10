dba433148421abe006921fcca3eceff13025c85e
Robin Wagner <robin-wagner@web.de>
2017-01-10 22:14:24 +0100

    Add steps for Twenty One game

--- programming_and_back-end_development/programming_foundations/lesson_4/twentyone.rb
+++ programming_and_back-end_development/programming_foundations/lesson_4/twentyone.rb
@@ -26,3 +26,14 @@
 #
 # Comparing cards: When both the player and the dealer stay, it's time to
 # compare the total value of the cards and see who has the highest value.
+
+# STEPS:
+# 1. Initialize deck
+# 2. Deal cards to player and dealer
+# 3. Player turn: hit or stay
+#   - repeat until bust or "stay"
+# 4. If player bust, dealer wins.
+# 5. Dealer turn: hit or stay
+#   - repeat until total >= 17
+# 6. If dealer bust, player wins.
+# 7. Compare cards and declare winner.
