11be811324ec471910b99f43e23256654485d596
Robin Wagner <robin-wagner@web.de>
2017-01-11 19:15:20 +0100

    Define top-level structure game

--- programming_and_back-end_development/programming_foundations/lesson_4/twentyone.rb
+++ programming_and_back-end_development/programming_foundations/lesson_4/twentyone.rb
@@ -37,3 +37,38 @@
 #   - repeat until total >= 17
 # 6. If dealer bust, player wins.
 # 7. Compare cards and declare winner.
+
+deck = [['H', '2'], ['H', '3'], ['H', '4'], ['H', '5'], ['H', '6'], ['H', '7'],
+        ['H', '8'], ['H', '9'], ['H', '10'], ['H', 'J'], ['H', 'Q'], ['H', 'K'],
+        ['H', 'A'], ['D', '2'], ['D', '3'], ['D', '4'], ['D', '5'], ['D', '6'],
+        ['D', '7'], ['D', '8'], ['D', '9'], ['D', '10'], ['D', 'J'], ['D', 'Q'],
+        ['D', 'K'], ['D', 'A'], ['C', '2'], ['C', '3'], ['C', '4'], ['C', '5'],
+        ['C', '6'], ['C', '7'], ['C', '8'], ['C', '9'], ['C', '10'], ['C', 'J'],
+        ['C', 'Q'], ['C', 'K'], ['C', 'A'], ['S', '2'], ['S', '3'], ['S', '4'],
+        ['S', '5'], ['S', '6'], ['S', '7'], ['S', '8'], ['S', '9'], ['S', '10'],
+        ['S', 'J'], ['S', 'Q'], ['S', 'K'], ['S', 'A']]
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+loop do
+  prompt "Welcome to Twenty-One!"
+
+  initialize_deck
+  deal_cards
+
+  player_deal_or_stay
+  dealer_turn
+
+  determine_winner
+
+  answer = ''
+  loop do
+    prompt "Play again? (Enter 'yes' or 'no')"
+    answer = gets.chomp.downcase
+    break if answer == 'yes' || answer == 'no'
+    prompt "Sorry, that's not a valid choice"
+  end
+  break unless answer.downcase.start_with?('y')
+end
