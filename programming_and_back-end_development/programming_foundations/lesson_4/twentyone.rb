9a601f26f0975ab16f1909a9a2b97bbf37ff000c
Robin Wagner <robin-wagner@web.de>
2017-01-14 12:29:55 +0100

    Replace magic numbers with constants

--- programming_and_back-end_development/programming_foundations/lesson_4/twentyone.rb
+++ programming_and_back-end_development/programming_foundations/lesson_4/twentyone.rb
@@ -1,43 +1,3 @@
-# Rules of Twenty-One
-
-# 52-card deck with 4 suits (hearts, diamonds, clubs, spades) and 13 values
-# (2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace).
-#
-# Goal: Get to 21 as close as possible without going over it.
-# If you go over 21, it's a "bust" and you lose.
-#
-# Setup: Game consists of a "dealer" and a "player". Both participants are
-# dealt 2 cards. The player can see their 2 cards, but can only see one of the
-# dealer's cards.
-#
-# Card values: Numbers 2 through 10 are worth their face value.
-# Jack, queen, and king are each worth 10
-# The ace is worth 1 or 11 (if the sum of the hand exceeds 21 then the ace must
-# be worth 1).
-#
-# Player turn: The player goes first, and can decide to either "hit" or "stay".
-# "Hit": A player will ask for another card. (when card value is more than 21,
-# player "busts" and loses). If the player busts, the game is over and the
-# dealer won.
-#
-# Dealer turn: When the player stays, it's the dealer's turn. The dealer follows
-# a strict rule: Hit until at least 17. If the player busts, the game is over
-# an the dealer won.
-#
-# Comparing cards: When both the player and the dealer stay, it's time to
-# compare the total value of the cards and see who has the highest value.
-
-# STEPS:
-# 1. Initialize deck
-# 2. Deal cards to player and dealer
-# 3. Player turn: hit or stay
-#   - repeat until bust or "stay"
-# 4. If player bust, dealer wins.
-# 5. Dealer turn: hit or stay
-#   - repeat until total >= 17
-# 6. If dealer bust, player wins.
-# 7. Compare cards and declare winner.
-
 
 require 'pry'
 
@@ -45,7 +5,12 @@ SUITS = ['H', 'D', 'S', 'C'].freeze
 VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J',
           'Q', 'K', 'A'].freeze
 
+MAXIMUM_WINNING_SCORE = 21
+DEALER_STOP_SCORE = 17
+
 REQUIRED_WINS = 5
+
+
 
 def prompt(message)
   puts "=> #{message}"
@@ -71,22 +36,22 @@ def total(cards)
 
   # correct for Aces
   values.select { |value| value == "A" }.count.times do
-    sum -= 10 if sum > 21
+    sum -= 10 if sum > MAXIMUM_WINNING_SCORE
   end
   sum
 end
 
 def busted?(cards)
-  total(cards) > 21
+  total(cards) > MAXIMUM_WINNING_SCORE
 end
 
 def detect_result(dealer_cards, player_cards)
   player_total = total(player_cards)
   dealer_total = total(dealer_cards)
 
-  if player_total > 21
+  if player_total > MAXIMUM_WINNING_SCORE
     :player_busted
-  elsif dealer_total > 21
+  elsif dealer_total > MAXIMUM_WINNING_SCORE
     :dealer_busted
   elsif dealer_total < player_total
     :player
@@ -151,10 +116,10 @@ def players_turn(player_cards, deck)
 end
 
 def dealers_turn(dealer_cards, deck)
-  prompt "Dealer turn..."
+  prompt "Dealer's turn..."
 
   loop do
-    break if busted?(dealer_cards) || total(dealer_cards) >= 17
+    break if busted?(dealer_cards) || total(dealer_cards) >= DEALER_STOP_SCORE
 
     prompt "Dealer hits!"
     dealer_cards << deck.pop
