28cf079ad5e2634f3b667fb4c1bf326d3a2aa270
Robin Wagner <robin-wagner@web.de>
2017-01-16 18:23:23 +0100

    Reverse output of array elements

--- programming_and_back-end_development/programming_foundations/lesson_4/loops_2/empty_the_array.rb
+++ programming_and_back-end_development/programming_foundations/lesson_4/loops_2/empty_the_array.rb
@@ -4,3 +4,12 @@ loop do
   puts names.shift
   break if names.empty?
 end
+
+
+# To print names from last to first: Use 'pop'
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+loop do
+  puts names.pop
+  break if names.empty?
+end
