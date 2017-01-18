013e1ca42606f170ee3185fa64eac933838681c8
Robin Wagner <robin-wagner@web.de>
2017-01-18 07:26:01 +0100

    Exercise 2 completed

---
+++ programming_and_back-end_development/programming_foundations/lesson_4/methods_more_methods_and_sorting/exercise2.rb
@@ -0,0 +1,6 @@
+['ant', 'bat', 'caterpillar'].count do |str|
+  str.length < 4
+end
+
+# returns the number of arguments for which the block returns true
+# Check out documentation for Array#count and test in irb
