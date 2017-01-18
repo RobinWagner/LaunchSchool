2a07cf5a3489e6fb75be34dc3b296be3c056b267
Robin Wagner <robin-wagner@web.de>
2017-01-18 07:38:09 +0100

    Exercise 5 completed

---
+++ programming_and_back-end_development/programming_foundations/lesson_4/methods_more_methods_and_sorting/exercise5.rb
@@ -0,0 +1,7 @@
+hash = { a: 'ant', b: 'bear' }
+hash.shift
+
+# consult documentation to find out
+# removes a key-value pair from hash and returns it as two-item array
+# => [:a, "ant"]
+# destructive method that mutates caller
