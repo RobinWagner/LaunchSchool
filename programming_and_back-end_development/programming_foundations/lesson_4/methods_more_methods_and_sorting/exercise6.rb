c507e74750e80dedfc9c0e8d97a2a863c7ec35ae
Robin Wagner <robin-wagner@web.de>
2017-01-18 07:40:34 +0100

    Exercise 6 completed

---
+++ programming_and_back-end_development/programming_foundations/lesson_4/methods_more_methods_and_sorting/exercise6.rb
@@ -0,0 +1,6 @@
+['ant', 'bear', 'caterpillar'].pop.size
+
+# => 11
+# pop returns last element from array (and mutates array)
+# size is called on returned value ('caterpillar') and counts number
+# of characters in string
