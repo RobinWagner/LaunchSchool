9c62d338d1f3390921c31884479f566cd80d8021
Robin Wagner <robin-wagner@web.de>
2017-01-19 21:23:23 +0100

    Exercise 11 completed

---
+++ programming_and_back-end_development/programming_foundations/lesson_4/additional_practice/exercise11.rb
@@ -0,0 +1,13 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" }
+}
+
+total = 0
+munsters.each do |name, criteria|
+  total += criteria["age"] if criteria["gender"] == "male"
+end
+
+total
