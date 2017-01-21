c1b82c6d87b95efa680d03754a9296b4da89d5d4
Robin Wagner <robin-wagner@web.de>
2017-01-21 16:20:50 +0100

    Exercise 10 completed

---
+++ programming_and_back-end_development/programming_foundations/ruby_basics/reading_documentation1/class_and_instance_methods.rb
@@ -0,0 +1,6 @@
+# File::path (class method): Returns the string representation of the path
+# File.path("/dev/ull") (called on class itself)
+# Ruby docs: http://ruby-doc.org/core-2.4.0/File.html#method-c-path
+# File#path (instance method): Returns pathname used to create file as string.
+# File.new("testfile").path  (called on object (instance of class))
+# Ruby docs: http://ruby-doc.org/core-2.4.0/File.html#method-i-path
