fbb605dd36209fbe908295c81c58d12058d574b2
Robin Wagner <robin-wagner@web.de>
2017-01-21 16:27:35 +0100

    Exercise 1 completed

---
+++ programming_and_back-end_development/programming_foundations/ruby_basics/reading_documentation2/methods_without_arguments.rb
@@ -0,0 +1,5 @@
+"xyz".upcase
+
+# String#upcase is String instance method called directly on a string object
+# (instance of class String)
+# Ruby docs: http://ruby-doc.org/core-2.4.0/String.html#method-i-upcase
