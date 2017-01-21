c1ab8a73974fc4039a54d759a1112d41933b6c47
Robin Wagner <robin-wagner@web.de>
2017-01-21 15:15:22 +0100

    Exercise 4 completed

---
+++ programming_and_back-end_development/programming_foundations/ruby_basics/reading_documentation1/return_value_break.rb
@@ -0,0 +1,3 @@
+# Documentation for break: https://ruby-doc.org/core-2.4.0/doc/syntax/control_expressions_rdoc.html#label-break+Statement
+# Value break sets the return value of the while loop to: nil if no argument
+# passed to break, or the value of the argument if an argument is given
