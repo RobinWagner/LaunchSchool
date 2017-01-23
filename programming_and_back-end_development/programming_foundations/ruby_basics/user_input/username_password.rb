81d4134f3a2344e62e69372752d5896febe6030a
Robin Wagner <robin-wagner@web.de>
2017-01-23 22:16:38 +0100

    Update exercise 7 solution

--- programming_and_back-end_development/programming_foundations/ruby_basics/user_input/username_password.rb
+++ programming_and_back-end_development/programming_foundations/ruby_basics/user_input/username_password.rb
@@ -7,9 +7,9 @@ loop do
 
   puts '>> Please enter password:'
   password_try = gets.chomp
-  
+
   break if user_name == USERNAME && password_try == PASSWORD
-  puts 'Authorization failed!'
+  puts '>> Authorization failed!'
 end
 
 puts 'Welcome!'
