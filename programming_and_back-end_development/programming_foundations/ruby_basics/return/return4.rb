39277d84f062666ec80816ae2da8b22c5b23b33b
Robin Wagner <robin-wagner@web.de>
2017-01-24 18:10:03 +0100

    Exercise 4 completed

---
+++ programming_and_back-end_development/programming_foundations/ruby_basics/return/return4.rb
@@ -0,0 +1,11 @@
+def meal
+  puts 'Dinner'
+  return 'Breakfast'
+end
+
+puts meal
+
+# 'Dinner'
+# 'Breakfast'
+# When meal is called it first prints out 'Dinner'. Then the return
+# value is passed to puts and printed out as well.
