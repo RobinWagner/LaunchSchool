c24747d4eea33d8f3cc3cd6403e316ab466496da
Robin Wagner <robin-wagner@web.de>
2017-01-24 18:05:09 +0100

    Exercise 2 completed

---
+++ programming_and_back-end_development/programming_foundations/ruby_basics/return/return2.rb
@@ -0,0 +1,9 @@
+def meal
+  'Evening'
+end
+
+puts meal
+
+# => 'Evening'
+# Method meal has no return statement, so it returns the last expression in the
+# method ('Evening'). The puts method prints the string.
