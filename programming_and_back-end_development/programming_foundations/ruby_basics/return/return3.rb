a25be86e3d3aabfe4d17a8933b0427dd55a93596
Robin Wagner <robin-wagner@web.de>
2017-01-24 18:07:10 +0100

    Exercise 3 completed

---
+++ programming_and_back-end_development/programming_foundations/ruby_basics/return/return3.rb
@@ -0,0 +1,10 @@
+def meal
+  return 'Breakfast'
+  'Dinner'
+end
+
+puts meal
+
+# => 'Breakfast'
+# When return statement is included in method, it is executed and not the last
+# line executed in the method.
