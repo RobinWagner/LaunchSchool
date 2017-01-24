c4720f241c3ade0a8fbcd4fec49a8103b6fb1b2e
Robin Wagner <robin-wagner@web.de>
2017-01-24 19:49:52 +0100

    Exercise 5 completed

---
+++ programming_and_back-end_development/programming_foundations/ruby_basics/conditionals/conditionals5.rb
@@ -0,0 +1,11 @@
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+# => My favorite number is 7.
+# Integers are truthy (evaluate to true but are not true themselves).
+# Only false and nil evaluate to true (everything else is truthy).
