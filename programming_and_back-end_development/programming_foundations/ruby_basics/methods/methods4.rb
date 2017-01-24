482dccbd8e78605bd7145ff6b1aab8e64e984c2c
Robin Wagner <robin-wagner@web.de>
2017-01-24 08:03:40 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/ruby_basics/methods/methods4.rb
+++ programming_and_back-end_development/programming_foundations/ruby_basics/methods/methods4.rb
@@ -1,11 +1,17 @@
-daylight = [true, false].sample
-
-def time_of_day(time)
-  if time
+def time_of_day(daytime)
+  if daytime
     puts 'It\'s daytime!'
   else
     puts 'It\'s nighttime!'
   end
 end
 
+daylight = [true, false].sample
 time_of_day(daylight)
+
+
+# Alternative:
+
+def time_of_day(daytime)
+  puts daytime ? 'It\'s daytime!' : 'It\'s nighttime!'
+end
