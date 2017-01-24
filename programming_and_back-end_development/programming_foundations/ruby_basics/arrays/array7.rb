a75742b227e848e7e9e62f3493c804b67c8513d1
Robin Wagner <robin-wagner@web.de>
2017-01-24 22:59:13 +0100

    Exercise 7 completed

---
+++ programming_and_back-end_development/programming_foundations/ruby_basics/arrays/array7.rb
@@ -0,0 +1,10 @@
+numbers = [5, 9, 21, 26, 39]
+
+divisible_by_three = numbers.select do |number|
+                       number % 3 == 0
+                     end
+p divisible_by_three
+
+# Alternative:
+divisible_by_three = numbers.select { |number| number % 3 == 0 }
+p divisible_by_three
