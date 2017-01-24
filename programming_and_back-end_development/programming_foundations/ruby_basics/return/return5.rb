12063f7614f6f149404a8faf9a5700bfe46385b4
Robin Wagner <robin-wagner@web.de>
2017-01-24 18:12:57 +0100

    Exercise 5 completed

---
+++ programming_and_back-end_development/programming_foundations/ruby_basics/return/return5.rb
@@ -0,0 +1,12 @@
+def meal
+  'Dinner'
+  puts 'Dinner'
+end
+
+p meal
+
+# Dinner
+# nil
+# When method is called 'Dinner' is printed. The return value of puts is
+# nil, therfore the p method prints nil to the screen. (p is used since puts
+# would print empty line)
