f06a07241e2ad59a21484fdc10101c5b9ea34693
Robin Wagner <robin-wagner@web.de>
2017-01-24 23:27:54 +0100

    Exercise 6 completed

---
+++ programming_and_back-end_development/programming_foundations/ruby_basics/hashes/hashes6.rb
@@ -0,0 +1,14 @@
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+half_numbers = numbers.map { |_, value| value / 2 }
+p half_numbers
+
+# Alternative syntax:
+half_numbers = numbers.map do |_, value|
+  value / 2
+end
+p half_numbers
