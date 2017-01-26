cbe726d88ade87101565ddb63cc2598ce6f21323
Robin Wagner <robin-wagner@web.de>
2017-01-26 19:02:36 +0100

    Exercise 3 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy1/easy3.rb
@@ -0,0 +1,31 @@
+def digit_list(number)
+  arr = []
+  while number > 0
+    arr << number % 10
+    number /= 10
+  end
+  arr.reverse
+end
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]
+puts digit_list(7) == [7]
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0]
+puts digit_list(444) == [4, 4, 4]
+
+
+# Alternative
+
+def digit_list(number)
+  digits = []
+  loop do
+    number, remainder = number.divmod(10)
+    digits.unshift(remainder)
+    break if number == 0
+  end
+  digits
+end
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]
+puts digit_list(7) == [7]
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0]
+puts digit_list(444) == [4, 4, 4]
