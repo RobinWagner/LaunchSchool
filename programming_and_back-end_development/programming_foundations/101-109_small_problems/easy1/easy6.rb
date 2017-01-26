323b84b6e227727638210540ea274a762c83f0d0
Robin Wagner <robin-wagner@web.de>
2017-01-26 22:34:31 +0100

    Alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy1/easy6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy1/easy6.rb
@@ -8,3 +8,17 @@ puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+
+
+# Alternative
+
+def stringy(size)
+  numbers = []
+
+  size.times do |index|
+    number = index.even? ? 1 : 0
+    numbers << number
+  end
+
+  numbers.join
+end
