1c0ba635f6f8599a2544f0c27c4ca41630e060a9
Robin Wagner <robin-wagner@web.de>
2017-01-26 21:49:07 +0100

    Alternative solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy1/easy4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy1/easy4.rb
@@ -13,3 +13,17 @@ def count_occurrences(elements)
 end
 
 count_occurrences(vehicles)
+
+# Alternative
+
+def count_occurrences(array)
+  occurrences = {}
+
+  array.uniq.each do |element|
+    occurrences[element] = array.count(element)
+  end
+
+  occurrences.each do |element, count|
+    puts "#{element} => #{count}"
+  end
+end
