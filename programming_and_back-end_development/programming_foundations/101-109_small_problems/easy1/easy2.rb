b8144c5acb52e5c69bc45c704ed3ced0d3623d07
Robin Wagner <robin-wagner@web.de>
2017-01-26 18:52:53 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy1/easy2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy1/easy2.rb
@@ -6,3 +6,13 @@ puts is_odd?(2)
 puts is_odd?(5)
 puts is_odd?(-17)
 puts is_odd?(-8)
+
+# Alternative:
+def is_odd?(number)
+  number.remainder(2) != 0
+end
+
+puts is_odd?(2)
+puts is_odd?(5)
+puts is_odd?(-17)
+puts is_odd?(-8)
