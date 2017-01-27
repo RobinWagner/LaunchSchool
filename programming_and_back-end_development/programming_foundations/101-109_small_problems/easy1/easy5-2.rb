0cde811969267219e91c8951e944f4f38f6c5e83
Robin Wagner <robin-wagner@web.de>
2017-01-27 23:43:30 +0100

    Additional solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy1/easy5-2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy1/easy5-2.rb
@@ -7,3 +7,20 @@ end
 p reverse_words('Professional')
 p reverse_words('Walk around the block')
 p reverse_words('Launch School')
+
+# Alternative solution:
+
+def reverse_words(string)
+  words = []
+
+  string.split.each do |word|
+    word.reverse! if word.size >= 5
+    words << word
+  end
+
+  words.join(' ')
+end
+
+p reverse_words('Professional')
+p reverse_words('Walk around the block')
+p reverse_words('Launch School')
