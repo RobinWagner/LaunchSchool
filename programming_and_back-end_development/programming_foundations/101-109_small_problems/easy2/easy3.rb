2410e76260b3bd916490d11ff029c87020fdae7d
Robin Wagner <robin-wagner@web.de>
2017-01-29 17:17:12 +0100

    Update solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy2/easy3.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy2/easy3.rb
@@ -1,12 +1,11 @@
 print "What is the bill? "
-amount_bill = gets.to_f
+amount_bill = gets.chomp.to_f
 
 print "What is the tip percentage? "
-tip = gets.to_f
+tip_percentage = gets.chomp.to_f
 
-amount_tip = amount_bill * (tip / 100)
-total = amount_bill + amount_tip
+amount_tip = (amount_bill * (tip_percentage / 100)).round(2)
+total      = (amount_bill + amount_tip).round(2)
 
-puts ""
-puts "The tip is $#{amount_tip.round(2)}"
+puts "The tip is $#{amount_tip}"
 puts "The total is $#{total}"
