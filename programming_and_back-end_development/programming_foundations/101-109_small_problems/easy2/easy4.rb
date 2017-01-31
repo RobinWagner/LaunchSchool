ebd5de08f3299cf4909116be70947b026f09e374
Robin Wagner <robin-wagner@web.de>
2017-01-31 18:09:47 +0100

    Initial structure exercise 4

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy2/easy4.rb
@@ -0,0 +1,11 @@
+# Input: tbd
+
+
+# Output:
+
+# Examples
+# What is your age? 30
+# At what age would you like to retire? 70
+#
+# It's 2016. You will retire in 2056.
+# You have only 40 years of work to go!
