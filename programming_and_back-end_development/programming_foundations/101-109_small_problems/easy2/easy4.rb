185e87d3e514e2751d434eaf0c2fab3330a31c6b
Robin Wagner <robin-wagner@web.de>
2017-02-01 17:51:33 +0100

    Exercise 4 completed

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy2/easy4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy2/easy4.rb
@@ -1,11 +1,14 @@
-# Input: tbd
+current_year = Time.new.year
 
+print 'What is your age? '
+age = gets.chomp
 
-# Output:
+print 'At what age would you like to retire? '
+retirement_age = gets.chomp
 
-# Examples
-# What is your age? 30
-# At what age would you like to retire? 70
-#
-# It's 2016. You will retire in 2056.
-# You have only 40 years of work to go!
+years_until_retirement = retirement_age.to_i - age.to_i
+retirement_year = current_year + years_until_retirement
+
+puts ''
+puts "It's #{current_year}. You will retire in #{retirement_year}."
+puts "You have only #{years_until_retirement} years of work to go!"
