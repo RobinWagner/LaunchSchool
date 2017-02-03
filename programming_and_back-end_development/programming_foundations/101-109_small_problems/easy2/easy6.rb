85d09fd5f3d69e4978b0ccdf9fcc4a66561e4618
Robin Wagner <robin-wagner@web.de>
2017-02-03 07:42:15 +0100

    Add alternative + fix solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy2/easy6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy2/easy6.rb
@@ -1,18 +1,29 @@
 # Print all odd numbers from 1 to 99, inclusive. All numbers should be printed on separate lines.
 
 # loop over numbers from 1 to 99
+## start with 1
 ## puts the number
+## increase number by 2
 
-count = 0
+count = 1
 
 loop do
-  count += 1
   puts count
-  break if count >= 99
+  count += 2
+  break if count > 99
 end
 
 # Alternative 1
 
 for i in 1..99 do
-  puts i
+  puts i if i.odd?
+end
+
+# Alternative 2
+
+count = 1
+
+while count <= 99 do
+  puts count
+  count += 2
 end
