0e82a4eb85559546fe888bab9535ce305e6172d8
Robin Wagner <robin-wagner@web.de>
2017-02-03 07:33:03 +0100

    Exercise 6 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy2/easy6.rb
@@ -0,0 +1,12 @@
+# Print all odd numbers from 1 to 99, inclusive. All numbers should be printed on separate lines.
+
+# loop over numbers from 1 to 99
+## puts the number
+
+count = 0
+
+loop do
+  count += 1
+  puts count
+  break if count >= 99
+end
