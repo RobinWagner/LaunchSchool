534b51d7289c7381d060d2bd86f8f70d990aa510
Robin Wagner <robin-wagner@web.de>
2017-02-06 07:33:48 +0100

    Exercise 8 completed

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy2/easy8.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy2/easy8.rb
@@ -1,3 +1,39 @@
 # Write a program that asks the user to enter an integer greater than 0,
 # then asks if the user wants to determine the sum or product of all
 # numbers between 1 and the entered integer.
+
+def compute_sum(number)
+  count = number
+  while count > 1
+    count -= 1
+    number += count
+  end
+  number
+end
+
+def compute_product(number)
+  count = number
+  while count > 1
+    count -= 1
+    number *= count
+  end
+  number
+end
+
+total = 0
+
+puts ">> Please enter an integer greater than 0:"
+number = gets.chomp.to_i
+
+puts ">> Enter 's' to compute the sum, 'p' to compute the product."
+input = gets.chomp
+
+if input == 's'
+  total = compute_sum(number)
+  puts "The sum of the integers between 1 and #{number} is #{total}."
+elsif input == 'p'
+  total = compute_product(number)
+  puts "The product of the integers between 1 and #{number} is #{total}."
+else
+  puts "Invalid input"
+end
