d6c837a3c34d6f0eeea60cbc680064fa8b43109e
Robin Wagner <robin-wagner@web.de>
2017-02-09 20:16:02 +0100

    Alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy3/easy2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy3/easy2.rb
@@ -14,3 +14,21 @@ else
 end
 puts "==> #{number1} % #{number2} = #{number1 % number2}"
 puts "==> #{number1} ** #{number2} = #{number1 ** number2}"
+
+# Alternative:
+
+def prompt(message)
+  puts "==> #{message}"
+end
+
+prompt("Enter the first number:")
+first_number = gets.chomp.to_i
+prompt("Enter the second number:")
+second_number = gets.chomp.to_i
+
+prompt("#{first_number} + #{second_number} = #{first_number + second_number}")
+prompt("#{first_number} - #{second_number} = #{first_number - second_number}")
+prompt("#{first_number} * #{second_number} = #{first_number * second_number}")
+prompt("#{first_number} / #{second_number} = #{first_number / second_number}")
+prompt("#{first_number} % #{second_number} = #{first_number % second_number}")
+prompt("#{first_number} ** #{second_number} = #{first_number**second_number}")
