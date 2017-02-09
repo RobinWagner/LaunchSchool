68893edefc8ad37e7a6ae0e2016f2468c547171e
Robin Wagner <robin-wagner@web.de>
2017-02-09 19:33:50 +0100

    Alternative solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy3/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy3/easy1.rb
@@ -24,3 +24,26 @@ final_number = gets.chomp.to_i
 
 puts "The number #{final_number} appears in #{result_array}." if result_array.include?(final_number)
 puts "The number #{final_number} does not appear in #{result_array}." unless result_array.include?(final_number)
+
+# Alternative:
+
+numbers = []
+
+puts "Enter the 1st number:"
+numbers << gets.chomp.to_i
+puts "Enter the 2nd number:"
+numbers << gets.chomp.to_i
+puts "Enter the 3rd number:"
+numbers << gets.chomp.to_i
+puts "Enter the 4th number:"
+numbers << gets.chomp.to_i
+puts "Enter the 5th number:"
+numbers << gets.chomp.to_i
+puts "Enter the last number:"
+last_number = gets.chomp.to_i
+
+if numbers.include? last_number
+  puts "The number #{last_number} appears in #{numbers}."
+else
+  puts "The number #{last_number} does not appear in #{numbers}."
+end
