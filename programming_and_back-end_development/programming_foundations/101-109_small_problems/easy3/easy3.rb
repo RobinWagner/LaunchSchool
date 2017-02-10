7543fe39a370d84e2fc22d789518f3d0ba33e1e4
Robin Wagner <robin-wagner@web.de>
2017-02-10 18:41:28 +0100

    Alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy3/easy3.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy3/easy3.rb
@@ -7,3 +7,10 @@ letters = word.split('')
 letters.each { |letter| result << letter if letter != ' '}
 
 puts "There are #{result.size} characters in \"#{word}\"."
+
+# Alternative solution:
+
+print 'Please write word or multiple words: '
+input = gets.chomp
+number_of_characters = input.delete(' ').size
+puts "There are #{number_of_characters} characters in \"#{input}\"."
