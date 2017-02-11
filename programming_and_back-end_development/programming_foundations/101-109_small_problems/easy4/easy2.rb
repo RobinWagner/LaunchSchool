c396552fae4049cd6aa42e152073da3b038657eb
Robin Wagner <robin-wagner@web.de>
2017-02-11 18:17:58 +0100

    Add solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy4/easy2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy4/easy2.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def century(year)
   if year.to_s[-1] == '0' && year.to_s[-2] == '0'
     (year / 100).to_s + ending((year / 100).to_s)
@@ -17,6 +15,36 @@ def ending(century)
     'rd'
   else
     'th'
+  end
+end
+
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
+
+# Alternative:
+
+def century(year)
+  century = year / 100 + 1
+  century -= 1 if year % 100 == 0
+  century.to_s + century_suffix(century)
+end
+
+def century_suffix(century)
+  return 'th' if [11, 12, 13].include?(century % 100)
+  last_digit = century % 10
+
+  case last_digit
+  when 1 then 'st'
+  when 2 then 'nd'
+  when 3 then 'rd'
+  else 'th'
   end
 end
 
