6cc513bdcb46b93387324cebe52276153c9c4638
Robin Wagner <robin-wagner@web.de>
2017-02-12 10:44:59 +0100

    Alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy4/easy5.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy4/easy5.rb
@@ -10,3 +10,14 @@ p multisum(3) == 3
 p multisum(5) == 8
 p multisum(10) == 33
 p multisum(1000) == 234168
+
+# Alternative
+
+def multisum(number)
+  (1..number).select { |n| (n % 3 == 0 || n % 5 == 0) }.inject(:+)
+end
+
+p multisum(3) == 3
+p multisum(5) == 8
+p multisum(10) == 33
+p multisum(1000) == 234168
