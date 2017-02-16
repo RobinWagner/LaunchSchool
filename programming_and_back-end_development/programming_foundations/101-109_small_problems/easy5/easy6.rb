1594fa0c1a53c847badd4116b3df9ce038109f00
Robin Wagner <robin-wagner@web.de>
2017-02-16 20:30:33 +0100

    Exercise 6 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy5/easy6.rb
@@ -0,0 +1,16 @@
+def word_sizes(words)
+  results = {}
+  words.split.each do |word|
+    if results[word.size]
+      results[word.size] += 1
+    else
+      results[word.size] = 1
+    end
+  end
+  results.sort.to_h
+end
+
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+p word_sizes('') == {}
