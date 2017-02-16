f00e8b8c3aea09384169d70d7bf1786eba59d30d
Robin Wagner <robin-wagner@web.de>
2017-02-16 12:53:36 +0100

    Add original solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy5/easy5.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy5/easy5.rb
@@ -1,5 +1,16 @@
-def cleanup(text)
-  text.gsub(/[^a-z]/i, ' ').squeeze(' ')
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z']
+
+def cleanup(string)
+  result = string.split('').map do |char|
+    if ALPHABET.include?(char)
+      char = char
+    else
+      char = ' '
+    end
+  end
+  result.join('').squeeze(' ')
 end
 
-p cleanup("---what's my +*& line?") == ' what s my line '
+p cleanup("---what's my +*& line?")
