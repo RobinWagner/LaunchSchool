e17ac17a0a507058795d5925f3cab26dec51e2b5
Robin Wagner <robin-wagner@web.de>
2017-02-16 12:36:17 +0100

    Alternative solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy5/easy4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy5/easy4.rb
@@ -10,3 +10,21 @@ end
 p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
 p swap('Abcde') == 'ebcdA'
 p swap('a') == 'a'
+
+# Alternative
+
+def swap_first_last_character(word)
+  word[0], word[-1] = word[-1], word[0]
+  word
+end
+
+def swap(words)
+  result = words.split.map do |word|
+    swap_first_last_character(word)
+  end
+  result.join(' ')
+end
+
+p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
+p swap('Abcde') == 'ebcdA'
+p swap('a') == 'a'
