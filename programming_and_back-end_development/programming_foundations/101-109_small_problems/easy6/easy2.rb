2ee8b3622febefa9a8e6179b2ebbda4373c9455a
Robin Wagner <robin-wagner@web.de>
2017-02-19 22:56:02 +0100

    Exercise 2 completed

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy6/easy2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy6/easy2.rb
@@ -2,6 +2,8 @@ def remove_vowels(strings)
   strings.map { |string| string.delete('aeiouAEIOU') }
 end
 
+# Test cases:
+
 p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
 p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
 p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
