f14e55323caaf2315e181da2aceddf36434e70d3
Robin Wagner <robin-wagner@web.de>
2017-02-21 13:03:39 +0100

    Exercise 4 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy6/easy4.rb
@@ -0,0 +1,18 @@
+# Input: Array
+# Output: Same array (mutated)
+
+# Algorithm:
+## Iterate over array
+
+def reverse!(array)
+  left_index = 0
+  right_index = -1
+
+  while left_index < array.size / 2
+    array[left_index], array[right_index] = array[right_index], array[left_index]
+    left_index += 1
+    right_index -= 1
+  end
+
+  array
+end
