972b9c84934353a337883c170b9970d329425e6f
Robin Wagner <robin-wagner@web.de>
2017-02-21 13:06:58 +0100

    Testcases added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy6/easy4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy6/easy4.rb
@@ -16,3 +16,22 @@ def reverse!(array)
 
   array
 end
+
+# Test cases
+
+list = [1,2,3,4]
+result = reverse!(list) # => [4,3,2,1]
+p list == [4, 3, 2, 1]
+p list.object_id == result.object_id
+
+list = %w(a b c d e)
+reverse!(list) # => ["e", "d", "c", "b", "a"]
+p list == ["e", "d", "c", "b", "a"]
+
+list = ['abc']
+reverse!(list) # => ["abc"]
+p list == ["abc"]
+
+list = []
+reverse!([]) # => []
+p list == []
