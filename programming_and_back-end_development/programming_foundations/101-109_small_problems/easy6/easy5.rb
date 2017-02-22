45ef5a02164087611dad4e8e9f5a33226a1bd9de
Robin Wagner <robin-wagner@web.de>
2017-02-22 13:28:03 +0100

    Exercise 5 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy6/easy5.rb
@@ -0,0 +1,26 @@
+# Input: array
+# Output: new array (don't mutate original array)
+
+def reverse(array)
+  new_array = []
+  length = array.size
+  index = -1
+  for n in 0..length-1 do
+    new_array[n] = array[index]
+    index -= 1
+  end
+  new_array
+end
+
+# Test cases:
+
+p reverse([1,2,3,4]) == [4,3,2,1]
+p reverse(%w(a b c d e)) == %w(e d c b a)
+p reverse(['abc']) == ['abc']
+p reverse([]) == []
+
+list = [1, 2, 3]
+new_list = reverse(list)
+p list.object_id != new_list.object_id
+p list == [1, 2, 3]
+p new_list == [3, 2, 1]
