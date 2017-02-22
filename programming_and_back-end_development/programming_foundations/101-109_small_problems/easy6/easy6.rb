5de6f1eec9bfebd105c9c2dcb92f8cf3fbdca6ec
Robin Wagner <robin-wagner@web.de>
2017-02-22 13:33:42 +0100

    Alternative solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy6/easy6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy6/easy6.rb
@@ -2,6 +2,12 @@ def merge(array1, array2)
   array1.concat(array2).uniq
 end
 
+# Alternative solution:
+
+def merge(array_1, array_2)
+  array_1 | array_2
+end
+
 # Test case:
 
 p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
