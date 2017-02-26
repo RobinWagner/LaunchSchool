f114dfdaec2bae8dc93431ca74041471b4f54b76
Robin Wagner <robin-wagner@web.de>
2017-02-26 15:08:57 +0100

    Exercise 9 completed

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy6/easy9.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy6/easy9.rb
@@ -1,10 +1,15 @@
-
+def include?(array, search_value)
+  array.each do |element|
+    return true if element == search_value
+  end
+  false
+end
 
 
 # Test cases:
 
-include?([1,2,3,4,5], 3) == true
-include?([1,2,3,4,5], 6) == false
-include?([], 3) == false
-include?([nil], nil) == true
-include?([], nil) == false
+p include?([1,2,3,4,5], 3) == true
+p include?([1,2,3,4,5], 6) == false
+p include?([], 3) == false
+p include?([nil], nil) == true
+p include?([], nil) == false
