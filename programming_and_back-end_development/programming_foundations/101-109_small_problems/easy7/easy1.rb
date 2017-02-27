99c78ae0c2b3c02746d4b4f699cca2fbdaee85f2
Robin Wagner <robin-wagner@web.de>
2017-02-27 12:16:11 +0100

    Shorter solution with zip method added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy7/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy7/easy1.rb
@@ -17,6 +17,12 @@ def interleave(array1, array2)
   result
 end
 
+# Alternative solution 2:
+
+def interleave(array1, array2)
+  array1.zip(array2).flatten
+end
+
 # Test case:
 
 p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
