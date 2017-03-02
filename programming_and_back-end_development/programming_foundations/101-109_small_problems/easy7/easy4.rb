7d0833f007ace6ada1847d5496cf0e29ce5281c9
Robin Wagner <robin-wagner@web.de>
2017-03-02 15:26:58 +0100

    Alternative solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy7/easy4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy7/easy4.rb
@@ -4,5 +4,22 @@ def swapcase(string)
   end.join
 end
 
+# Alternative solution:
+
+def swapcase(string)
+  characters = string.chars.map do |char|
+    if char =~ /[a-z]/
+      char.upcase
+    elsif char =~ /[A-Z]/
+      char.downcase
+    else
+      char
+    end
+  end
+  characters.join
+end
+
+# Test cases:
+
 p swapcase('CamelCase') == 'cAMELcASE'
 p swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
