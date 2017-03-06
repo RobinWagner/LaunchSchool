1bfbe4610772db5eee38b59442f9b7713542b09a
Robin Wagner <robin-wagner@web.de>
2017-03-06 22:06:01 +0100

    Alternative solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy7/easy8.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy7/easy8.rb
@@ -6,4 +6,16 @@ def multiply_list(list1, list2)
   result
 end
 
+# Alternative solution:
+
+def multiply_list(list_1, list_2)
+  products = []
+  list_1.each_with_index do |item, index|
+    products << item * list_2[index]
+  end
+  products
+end
+
+# Test case:
+
 p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
