1dbacf4ce7bb98246888c9154df636b21e1a2cbb
Robin Wagner <robin-wagner@web.de>
2017-03-06 22:12:09 +0100

    Alternative solution 2 added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy7/easy9.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy7/easy9.rb
@@ -19,5 +19,10 @@ def multiply_all_pairs(array_1, array_2)
   products.sort
 end
 
+# Alternative solution 2:
+def multiply_all_pairs(array_1, array_2)
+  array_1.product(array_2).map { |num1, num2| num1 * num2 }.sort
+end
+
 # Test case:
 p multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
