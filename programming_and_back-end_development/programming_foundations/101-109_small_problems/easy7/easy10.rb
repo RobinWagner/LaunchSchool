ac7854826fccc72d97e50187c851e2d5311cedce
Robin Wagner <robin-wagner@web.de>
2017-03-06 22:16:45 +0100

    Alternative solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy7/easy10.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy7/easy10.rb
@@ -2,5 +2,12 @@ def penultimate(words)
   words.split(' ')[-2]
 end
 
+# Alternative solution:
+def penultimate(words)
+  words_array = words.split
+  words_array[-2]
+end
+
+# Test cases:
 p penultimate('last word') == 'last'
 p penultimate('Launch School is great!') == 'is'
