6d335e8265abe5e78195f5b0df8e954a4112ab98
Robin Wagner <robin-wagner@web.de>
2017-03-07 22:10:14 +0100

    Exercise 8 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy8.rb
@@ -0,0 +1,20 @@
+CONSONANTS = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
+              'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+
+def double_consonants(string)
+  result = ''
+  0.upto(string.size - 1) do |index|
+    if CONSONANTS.include?(string[index].downcase)
+      result += string[index] * 2
+    else
+      result += string[index]
+    end
+  end
+  result
+end
+
+# Test cases:
+p double_consonants('String') == "SSttrrinngg"
+p double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
+p double_consonants("July 4th") == "JJullyy 4tthh"
+p double_consonants('') == ""
