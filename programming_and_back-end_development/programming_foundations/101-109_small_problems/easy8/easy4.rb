522e06a37caf3cfacb7828efc8fad9d6ec7b5c8e
Robin Wagner <robin-wagner@web.de>
2017-03-07 20:31:00 +0100

    Alternative solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy4.rb
@@ -13,6 +13,26 @@ def substrings_at_start(string)
   substrings
 end
 
+
+
+# Alternative solution:
+def substrings(string)
+  results = []
+  (0...string.size).each do |start_index|
+    this_substring = string[start_index..-1]
+    results.concat(substrings_at_start(this_substring))
+  end
+  results
+end
+
+def substrings_at_start(string)
+  result = []
+  0.upto(string.size - 1) do |index|
+    result << string[0..index]
+  end
+  result
+end
+
 # Test case:
 p substrings('abcde') == [
   'a', 'ab', 'abc', 'abcd', 'abcde',
