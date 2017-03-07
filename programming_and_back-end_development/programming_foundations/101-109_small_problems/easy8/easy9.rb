0eeab290357217deb9d8258037176a733367a949
Robin Wagner <robin-wagner@web.de>
2017-03-07 22:21:12 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy9.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy9.rb
@@ -2,6 +2,16 @@ def reversed_number(number)
   number.to_s.reverse.to_i
 end
 
+# Alternative solution:
+def reversed_number(number)
+  string = number.to_s
+  digits = string.chars
+  reversed_digits = digits.reverse
+  reversed_string = reversed_digits.join('')
+  reversed_string.to_i
+end
+
+# Test cases:
 p reversed_number(12345) == 54321
 p reversed_number(12213) == 31221
 p reversed_number(456) == 654
