55ea3e008fa2eba2f5854ef02a1fe15786f2a2cd
Robin Wagner <robin-wagner@web.de>
2017-03-07 12:39:30 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy3.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy3.rb
@@ -6,6 +6,16 @@ def substrings_at_start(string)
   substrings
 end
 
+# Alternative solution:
+def substrings_at_start(string)
+  result = []
+  0.upto(string.size - 1) do |index|
+    result << string[0..index]
+  end
+  result
+end
+
+# Test cases:
 p substrings_at_start('abc') == ['a', 'ab', 'abc']
 p substrings_at_start('a') == ['a']
 p substrings_at_start('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
