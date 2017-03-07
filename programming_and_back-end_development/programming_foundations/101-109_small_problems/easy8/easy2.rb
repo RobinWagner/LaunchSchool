96da5d36aa84cdc8927eae90f8f107cbe16a36bd
Robin Wagner <robin-wagner@web.de>
2017-03-07 12:34:24 +0100

    Extend solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy2.rb
@@ -10,5 +10,8 @@ adjective = gets.chomp
 print "Enter an adverb: "
 adverb = gets.chomp
 
-puts ""
-puts "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
+sentence_1 = "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
+sentence_2 = "The #{adjective} #{noun} #{verb}s #{adverb} over the lazy dog."
+sentence_3 = "The #{noun} #{adverb} #{verb}s up #{adjective} Joe's turtle."
+
+puts [sentence_1, sentence_2, sentence_3].sample
