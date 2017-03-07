e72987e3e30e12b576d4b0ff284bd50b0312c105
Robin Wagner <robin-wagner@web.de>
2017-03-07 22:29:54 +0100

    Add longer solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy10.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy10.rb
@@ -2,6 +2,17 @@ def center_of(string)
   string.length.odd? ? string[string.size / 2] : string[string.size / 2 - 1, 2]
 end
 
+# Alternative solution:
+def center_of(string)
+  center_index = string.size / 2
+  if string.size.odd?
+    string[center_index]
+  else
+    string[center_index - 1, 2]
+  end
+end
+
+# Test cases:
 p center_of('I love ruby') == 'e'
 p center_of('Launch School') == ' '
 p center_of('Launch') == 'un'
