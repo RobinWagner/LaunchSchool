66d9d0d6730552a4fabd632257acf93c65972b6a
Robin Wagner <robin-wagner@web.de>
2017-03-07 22:13:31 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy8.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy8.rb
@@ -1,5 +1,4 @@
-CONSONANTS = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
-              'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+CONSONANTS = %w(b c d f g h j k l m n p q r s t v w x y z)
 
 def double_consonants(string)
   result = ''
@@ -9,6 +8,16 @@ def double_consonants(string)
     else
       result += string[index]
     end
+  end
+  result
+end
+
+# Alternative solution:
+def double_consonants(string)
+  result = ''
+  string.each_char do |char|
+    result << char
+    result << char if CONSONANTS.include?(char.downcase)
   end
   result
 end
