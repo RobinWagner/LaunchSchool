4ba2a75bd508e8b7dd9d31ce0c79d41c6f66eb63
Robin Wagner <robin-wagner@web.de>
2017-03-07 21:55:41 +0100

    Alternative solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy8/easy6.rb
@@ -12,4 +12,27 @@ def fizzbuzz(num_1, num_2)
   end
 end
 
+# Alternative solution:
+def fizzbuzz(starting_number, ending_number)
+  result = []
+  starting_number.upto(ending_number) do |number|
+    result << fizzbuzz_value(number)
+  end
+  puts result.join(', ')
+end
+
+def fizzbuzz_value(number)
+  case
+  when number % 3 == 0 && number % 5 == 0
+    'FizzBuzz'
+  when number % 5 == 0
+    'Buzz'
+  when number % 3 == 0
+    'Fizz'
+  else
+    number
+  end
+end
+
+# Test case:
 fizzbuzz(1, 15)
