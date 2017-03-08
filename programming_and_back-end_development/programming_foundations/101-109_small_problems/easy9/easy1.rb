46cf81820585f832b5a906e67a1ed000711117f0
Robin Wagner <robin-wagner@web.de>
2017-03-08 23:24:59 +0100

    Shorten line

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy1.rb
@@ -5,7 +5,8 @@ end
 
 # Alternative:
 def greetings(name, status)
-  puts "Hello, #{name.join(' ')}! Nice to have a #{status[:title]} #{status[:occupation]} around."
+  puts "Hello, #{name.join(' ')}! " \
+      "Nice to have a #{status[:title]} #{status[:occupation]} around."
 end
 
 greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
