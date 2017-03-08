57840a9c6c91e856857f9ff6e2421acfabfecc6f
Robin Wagner <robin-wagner@web.de>
2017-03-08 23:24:05 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy1.rb
@@ -3,4 +3,9 @@ def greetings(name, info)
        " Nice to have a #{info[:title]} #{info[:occupation]} around."
 end
 
+# Alternative:
+def greetings(name, status)
+  puts "Hello, #{name.join(' ')}! Nice to have a #{status[:title]} #{status[:occupation]} around."
+end
+
 greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
