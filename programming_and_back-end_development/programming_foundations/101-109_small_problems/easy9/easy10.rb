412b45dfeefeb30af5dc39230ebf86a43ed1ad3a
Robin Wagner <robin-wagner@web.de>
2017-03-09 20:15:50 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy10.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy10.rb
@@ -6,5 +6,11 @@ def buy_fruit(items)
   result
 end
 
+# Alternative:
+def buy_fruit(list)
+  list.map { |fruit, quantity| [fruit] * quantity }.flatten
+end
+
+# Test cases:
 p buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) ==
   ["apples", "apples", "apples", "orange", "bananas","bananas"]
