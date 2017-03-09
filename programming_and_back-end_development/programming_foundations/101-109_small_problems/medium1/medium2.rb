a12cc29d355bf2103c95855506a57d5044746d76
Robin Wagner <robin-wagner@web.de>
2017-03-09 21:50:13 +0100

    Exercise 2 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/medium1/medium2.rb
@@ -0,0 +1,18 @@
+def rotate_rightmost_digits(number, digits)
+  num_string = number.to_s
+  not_rotated = num_string[0, num_string.size - digits]
+  rotated = num_string[num_string.size - digits, num_string.size]
+  result_string = not_rotated + rotate_array(rotated.split('')).join
+  result_string.to_i
+end
+
+def rotate_array(array)
+  array[1..-1] + [array[0]]
+end
+
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
+p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
