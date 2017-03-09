88e170ffed0882f2d61cb2f20664d6c10828b74a
Robin Wagner <robin-wagner@web.de>
2017-03-09 19:53:21 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy8.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy8.rb
@@ -4,6 +4,12 @@ def sequence(count, sequence)
   result
 end
 
+# Alternative:
+def sequence(count, first)
+  (1..count).map { |idx| idx * first }
+end
+
+# Test cases:
 p sequence(5, 1) == [1, 2, 3, 4, 5]
 p sequence(4, -7) == [-7, -14, -21, -28]
 p sequence(3, 0) == [0, 0, 0]
