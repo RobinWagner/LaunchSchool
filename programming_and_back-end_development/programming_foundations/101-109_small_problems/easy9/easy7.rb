a816869580991a77ed70f9215ae1bc00d74b3d1f
Robin Wagner <robin-wagner@web.de>
2017-03-09 19:45:47 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy7.rb
@@ -2,4 +2,10 @@ def swap_name(name)
   "#{name.split[1]}, #{name.split[0]}"
 end
 
+# Alternative:
+def swap_name(name)
+  name.split(' ').reverse.join(', ')
+end
+
+# Test case:
 p swap_name('Joe Roberts') == 'Roberts, Joe'
