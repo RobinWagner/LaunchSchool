1ed58dc86c17e4cb6033a34a726c2d3264351b19
Robin Wagner <robin-wagner@web.de>
2017-03-09 19:26:41 +0100

    Exercise 5 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/easy9/easy5.rb
@@ -0,0 +1,11 @@
+def uppercase?(string)
+  string == string.upcase ? true : false
+end
+
+# Test cases:
+p uppercase?('t') == false
+p uppercase?('T') == true
+p uppercase?('Four Score') == false
+p uppercase?('FOUR SCORE') == true
+p uppercase?('4SCORE!') == true
+p uppercase?('') == true
