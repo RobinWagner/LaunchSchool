d7cb96cc5eca7ddbf0ed4ca52fb3ccd0139abe0e
Robin Wagner <robin-wagner@web.de>
2017-03-10 07:53:55 +0100

    Fix typo

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/medium1/medium2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/medium1/medium2.rb
@@ -10,7 +10,7 @@ def rotate_array(array)
   array[1..-1] + [array[0]]
 end
 
-# Alernative solution:
+# Alternative solution:
 def rotate_rightmost_digits(number, n)
   all_digits = number.to_s.chars
   all_digits[-n..-1] = rotate_array(all_digits[-n..-1])
