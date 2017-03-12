19811844297b17b745d7ec04411c65dd7b3c96e1
Robin Wagner <robin-wagner@web.de>
2017-03-12 13:54:23 +0100

    Add tail recursion solution (much more performant)

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/medium1/medium8.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/medium1/medium8.rb
@@ -10,6 +10,21 @@ def fibonacci(nth)
   fibonacci(nth - 1) + fibonacci(nth - 2)
 end
 
+# Alternative solution 2 (tail recursion):
+def fibonacci_tail(nth, acc1, acc2)
+  if nth == 1
+    acc1
+  elsif nth == 2
+    acc2
+  else
+    fibonacci_tail(nth - 1, acc2, acc2 + acc1)
+  end
+end
+
+def fibonacci(nth)
+  fibonacci_tail(nth, 1, 1)
+end
+
 # Test cases:
 p fibonacci(1) == 1
 p fibonacci(2) == 1
@@ -18,3 +33,5 @@ p fibonacci(4) == 3
 p fibonacci(5) == 5
 p fibonacci(12) == 144
 p fibonacci(20) == 6765
+p fibonacci(50)
+p fibonacci(8000)
