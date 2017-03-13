bd35327690f3f666fdfc2d9e99adf22e5ba5979d
Robin Wagner <robin-wagner@web.de>
2017-03-13 18:01:16 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/medium2/medium6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/medium2/medium6.rb
@@ -13,6 +13,22 @@ def triangle(angle1, angle2, angle3)
   end
 end
 
+# Alternative solution:
+def triangle(angle1, angle2, angle3)
+  angles = [angle1, angle2, angle3]
+
+  case
+  when angles.reduce(:+) != 180, angles.include?(0)
+    :invalid
+  when angles.include?(90)
+    :right
+  when angles.all? { |angle| angle < 90 }
+    :acute
+  else
+    :obtuse
+  end
+end
+
 # Test cases:
 p triangle(60, 70, 50) == :acute
 p triangle(30, 90, 60) == :right
