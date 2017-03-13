35f45e896c5fdc02742a477833354f47d102e9c0
Robin Wagner <robin-wagner@web.de>
2017-03-13 18:28:47 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems/medium2/medium7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems/medium2/medium7.rb
@@ -8,6 +8,18 @@ def friday_13th?(year)
   count
 end
 
+# Alternative solution:
+def friday_13th?(year)
+  unlucky_count = 0
+  thirteenth = Date.new(year, 1, 13)
+  12.times do
+    unlucky_count += 1 if thirteenth.friday?
+    thirteenth = thirteenth.next_month
+  end
+  unlucky_count
+end
+
+# Test cases:
 p friday_13th?(2015) == 3
 p friday_13th?(1986) == 1
 p friday_13th?(2019) == 2
