bf58d1533f6341f43731b19650e69bd62bcd3f5c
Robin Wagner <robin-wagner@web.de>
2017-03-14 20:37:13 +0100

    Add small improvement

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy2.rb
@@ -1,7 +1,11 @@
-puts '==> Enter the first number:'
+def prompt(message)
+  puts "==> #{message}"
+end
+
+prompt("Enter the first number:")
 number1 = gets.chomp.to_i
 
-puts '==> Enter the second number:'
+prompt("Enter the second number:")
 number2 = gets.chomp.to_i
 
 puts "==> #{number1} + #{number2} = #{number1 + number2}"
