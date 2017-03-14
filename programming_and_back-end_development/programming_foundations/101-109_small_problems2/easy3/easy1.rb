69359f828ca5f6dc482b650e96f0f56ebf7709ac
Robin Wagner <robin-wagner@web.de>
2017-03-14 20:31:51 +0100

    Alternative solution added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy1.rb
@@ -25,3 +25,26 @@ last = gets.chomp.to_i
 
 puts arr.include?(last) ? "The number #{last} appears in #{arr}."
                         : "The number #{last} does not appear in #{arr}."
+
+# Alternative:
+
+numbers = []
+
+puts "Enter the 1st number:"
+numbers << gets.chomp.to_i
+puts "Enter the 2nd number:"
+numbers << gets.chomp.to_i
+puts "Enter the 3rd number:"
+numbers << gets.chomp.to_i
+puts "Enter the 4th number:"
+numbers << gets.chomp.to_i
+puts "Enter the 5th number:"
+numbers << gets.chomp.to_i
+puts "Enter the last number:"
+last_number = gets.chomp.to_i
+
+if numbers.include? last_number
+  puts "The number #{last_number} appears in #{numbers}."
+else
+  puts "The number #{last_number} appears in #{numbers}."
+end
