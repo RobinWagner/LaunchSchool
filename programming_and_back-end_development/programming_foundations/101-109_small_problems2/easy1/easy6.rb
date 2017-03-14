74886e7ad7557d5533a2f6554d6397dde9888aea
Robin Wagner <robin-wagner@web.de>
2017-03-14 07:17:40 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy6.rb
@@ -2,7 +2,19 @@ def reverse_words(words)
   words.split.map { |word| word.size >= 5 ? word.reverse : word }.join(' ')
 end
 
+# Alternative solution:
+def reverse_words(string)
+  words = []
+
+  string.split.each do |word|
+    word.reverse! if word.size >= 5
+    words << word
+  end
+
+  words.join(' ')
+end
+
 # Test cases:
 puts reverse_words('Professional') == 'lanoisseforP'
 puts reverse_words('Walk around the block') == 'Walk dnuora the kcolb'
-puts reverse_words('Launch School') == 'hcnuaL loohcS' 
+puts reverse_words('Launch School') == 'hcnuaL loohcS'
