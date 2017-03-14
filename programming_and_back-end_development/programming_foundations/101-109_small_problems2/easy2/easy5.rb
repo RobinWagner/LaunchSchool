7b3690ab52f593a86d6764a08f4947dbbcc71e68
Robin Wagner <robin-wagner@web.de>
2017-03-14 19:39:33 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy2/easy5.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy2/easy5.rb
@@ -1,4 +1,15 @@
-print "What is your name? "
+print 'What is your name? '
 n = gets.chomp
 
 puts n.include?('!') ? "HELLO #{n}. WHY ARE WE SCREAMING?" : "Hello #{n}."
+
+# Alternative solution
+print 'What is your name? '
+name = gets.chomp
+
+if name[-1] == '!'
+  name = name.chop
+  puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
