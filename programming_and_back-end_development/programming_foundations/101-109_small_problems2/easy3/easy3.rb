9c701155236a10d47cf78289b137fc35484c8931
Robin Wagner <robin-wagner@web.de>
2017-03-14 20:43:57 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy3.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy3.rb
@@ -3,3 +3,9 @@ print 'Please write word or multiple words: '
 word = gets.chomp
 
 puts "There are #{word.split.join.size} characters in \"#{word}\""
+
+# Alternative solution
+print 'Please write word or multiple words: '
+input = gets.chomp
+number_of_characters = input.delete(' ').size
+puts "There are #{number_of_characters} characters in \"#{input}\"."
