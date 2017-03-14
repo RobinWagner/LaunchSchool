491312f1bef1a9b93b2362dca165c34e66780868
Robin Wagner <robin-wagner@web.de>
2017-03-14 19:41:44 +0100

    Change to destructive methods

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy2/easy5.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy2/easy5.rb
@@ -5,10 +5,10 @@ puts n.include?('!') ? "HELLO #{n}. WHY ARE WE SCREAMING?" : "Hello #{n}."
 
 # Alternative solution
 print 'What is your name? '
-name = gets.chomp
+name = gets.chomp!
 
 if name[-1] == '!'
-  name = name.chop
+  name = name.chop!
   puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
 else
   puts "Hello #{name}."
