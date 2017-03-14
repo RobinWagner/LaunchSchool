3c77f5f1a35a337e121891c352183aa37834e63d
Robin Wagner <robin-wagner@web.de>
2017-03-14 07:22:24 +0100

    Exercise 7 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy7.rb
@@ -0,0 +1,13 @@
+def stringy(number)
+  result = []
+  1.upto(number) do |index|
+    index.odd? ? result << '1' : result << '0'
+  end
+  result.join('')
+end
+
+# Test cases
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
