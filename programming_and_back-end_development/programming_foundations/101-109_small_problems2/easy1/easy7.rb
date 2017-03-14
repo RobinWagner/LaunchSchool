e7d22867382f1591a7f3d42049a4c478489a636e
Robin Wagner <robin-wagner@web.de>
2017-03-14 07:31:11 +0100

    Add further exploration solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy7.rb
@@ -17,8 +17,23 @@ def stringy(size)
   numbers.join
 end
 
+# Further Exploration:
+def stringy(number, setting=1)
+  result = []
+  setting == 0 ? first = 0 : first = 1
+  setting == 0 ? second = 1 : second = 0
+  1.upto(number) do |index|
+    index.odd? ? result << first : result << second
+  end
+  result.join('')
+end
+
 # Test cases
 puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+
+puts stringy(7, 1) == '1010101'
+puts stringy(4, 0) == '0101'
+puts stringy(7, 0) == '0101010'
