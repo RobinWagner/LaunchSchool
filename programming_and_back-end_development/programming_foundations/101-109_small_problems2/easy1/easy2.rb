393e354ead6a7e2cb11d5263b63a56900ad25d21
Robin Wagner <robin-wagner@web.de>
2017-03-14 06:28:09 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy2.rb
@@ -2,6 +2,11 @@ def is_odd?(integer)
   integer % 2 != 0
 end
 
+# Alternative solution:
+def is_odd?(number)
+  number % 2 == 1
+end
+
 # Test cases:
 puts is_odd?(2) == false    # => false
 puts is_odd?(5) == true   # => true
