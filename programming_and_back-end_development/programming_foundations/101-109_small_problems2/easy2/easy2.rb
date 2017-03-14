a602227df9f04e3da164c2f4a7671d36d45ef4af
Robin Wagner <robin-wagner@web.de>
2017-03-14 08:00:32 +0100

    Exercise 2 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy2/easy2.rb
@@ -0,0 +1,11 @@
+puts "Enter the length of the room in meters: "
+length_meters = gets.chomp.to_f
+
+puts "Enter the width of the room in meters: "
+width_meters = gets.chomp.to_f
+
+area_meters = length_meters * width_meters
+area_feet = area_meters * 10.7639
+
+puts "The area of the room is #{area_meters} square meters " \
+     "(#{area_feet.round(2)} square feet)."
