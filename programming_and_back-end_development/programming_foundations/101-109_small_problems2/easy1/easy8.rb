e850ee0688507df42b20d074b66e429ed79ab534
Robin Wagner <robin-wagner@web.de>
2017-03-14 07:37:43 +0100

    Add solution for further exploration

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy8.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy8.rb
@@ -1,13 +1,13 @@
 def average(numbers)
-  numbers.inject(:+) / numbers.size
+  numbers.inject(:+) / (numbers.size.to_f)
 end
 
 # Alternative solution
 def average(numbers)
   sum = numbers.reduce { |sum, number| sum + number }
-  sum / numbers.count
+  sum / (numbers.count.to_f)
 end
 
 # Test cases
-puts average([1, 5, 87, 45, 8, 8]) == 25
-puts average([9, 47, 23, 95, 16, 52]) == 40
+puts average([1, 5, 87, 45, 8, 8])# == 25
+puts average([9, 47, 23, 95, 16, 52])# == 40
