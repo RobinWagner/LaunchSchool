83818638d8bb8de3f065692c7707f15a1cf98f5c
Robin Wagner <robin-wagner@web.de>
2017-03-14 07:25:20 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy7.rb
@@ -6,6 +6,17 @@ def stringy(number)
   result.join('')
 end
 
+# Alternative solution:
+def stringy(size)
+  numbers = []
+
+  size.times do |index|
+    number = index.even? ? 1 : 0
+    numbers << number
+  end
+  numbers.join
+end
+
 # Test cases
 puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
