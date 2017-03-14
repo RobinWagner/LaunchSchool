b78b6fbab10b123f13f672ffe8a966f7a56054ee
Robin Wagner <robin-wagner@web.de>
2017-03-14 08:02:16 +0100

    Refactor solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy2/easy2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy2/easy2.rb
@@ -1,11 +1,13 @@
+SQMETERS_TO_SQFEET = 10.7639
+
 puts "Enter the length of the room in meters: "
 length_meters = gets.chomp.to_f
 
 puts "Enter the width of the room in meters: "
 width_meters = gets.chomp.to_f
 
-area_meters = length_meters * width_meters
-area_feet = area_meters * 10.7639
+area_meters = (length_meters * width_meters).round(2)
+area_feet = (area_meters * SQMETERS_TO_SQFEET).round(2)
 
 puts "The area of the room is #{area_meters} square meters " \
-     "(#{area_feet.round(2)} square feet)."
+     "(#{area_feet} square feet)."
