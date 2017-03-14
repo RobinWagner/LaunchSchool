f71c97054a67d83e16da57f2deefa39904ee9d9f
Robin Wagner <robin-wagner@web.de>
2017-03-14 06:33:07 +0100

    Add second alternative

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy1/easy2.rb
@@ -7,6 +7,11 @@ def is_odd?(number)
   number % 2 == 1
 end
 
+# Alternative solution 2:
+def is_odd?(number)
+  number.remainder(2) != 0
+end
+
 # Test cases:
 puts is_odd?(2) == false    # => false
 puts is_odd?(5) == true   # => true
