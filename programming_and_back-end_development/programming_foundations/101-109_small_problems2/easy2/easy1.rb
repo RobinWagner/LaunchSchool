27a29f4ff9169ec2b304bf28c42787daffb42354
Robin Wagner <robin-wagner@web.de>
2017-03-14 07:56:18 +0100

    Add solution for further exploration

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy2/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy2/easy1.rb
@@ -3,3 +3,15 @@ puts "Teddy is #{rand(20..200)} years old!"
 # Alternative solution
 age = rand(20..200)
 puts "Teddy is #{age} years old!"
+
+# Further exploration
+def print_age(name)
+  age = rand(20..200)
+  name.strip.size > 0 ? name : name = "Teddy"
+  puts "#{name} is #{age} years old!"
+end
+
+print "Enter a name: "
+name = gets.chomp
+
+print_age(name)
