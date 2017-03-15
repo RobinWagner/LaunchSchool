acc52b956f2ca38e705f7a18c7a115438ddc4de5
Robin Wagner <robin-wagner@web.de>
2017-03-15 07:36:48 +0100

    Add solution for further exploration

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy8.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy8.rb
@@ -1,5 +1,5 @@
-def palindrome?(string)
-  string == string.reverse
+def palindrome?(object)
+  object == object.reverse
 end
 
 # Test cases
@@ -7,3 +7,4 @@ p palindrome?('madam') == true
 p palindrome?('Madam') == false          # (case matters)
 p palindrome?("madam i'm adam") == false # (all characters matter)
 p palindrome?('356653') == true
+p palindrome?('madam') == true
