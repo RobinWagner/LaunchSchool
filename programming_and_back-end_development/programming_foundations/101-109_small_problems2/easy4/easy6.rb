b484677ef1b5a74279f170873680aab81e56c397
Robin Wagner <robin-wagner@web.de>
2017-03-15 20:57:56 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy6.rb
@@ -7,6 +7,12 @@ def running_total(array)
   array
 end
 
+# Alternative solution
+def running_total(array)
+  sum = 0
+  array.map { |value| sum += value }
+end
+
 # Test cases
 p running_total([2, 5, 13]) == [2, 7, 20]
 p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
