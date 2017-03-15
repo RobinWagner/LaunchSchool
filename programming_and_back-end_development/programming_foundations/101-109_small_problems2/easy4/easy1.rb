1e1a1d4a91c7ec7712f3c6818bc38e300afd3dd9
Robin Wagner <robin-wagner@web.de>
2017-03-15 18:29:01 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy1.rb
@@ -10,6 +10,15 @@ def short_long_short(string1, string2)
   min + max + min
 end
 
+# Alternative solution
+def short_long_short(string1, string2)
+  if string1.length > string2.length
+    string2 + string1 + string2
+  else
+    string1 + string2 + string1
+  end
+end
+
 # Test cases
 p short_long_short('abc', 'defgh') == "abcdefghabc"
 p short_long_short('abcde', 'fgh') == "fghabcdefgh"
