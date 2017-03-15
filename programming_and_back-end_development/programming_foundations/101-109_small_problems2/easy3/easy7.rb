b01097b73ae0ae5606ae12b7fc9e85fe7d3cc8f4
Robin Wagner <robin-wagner@web.de>
2017-03-15 07:25:57 +0100

    Add solution for further exploration

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy7.rb
@@ -26,7 +26,22 @@ def oddities(array)
   odd_elements
 end
 
+# Further exploration
+def evens(array)
+  result = []
+  0.upto(array.size - 1) do |index|
+    result << array[index] if index.odd?
+  end
+  result
+end
+
+# Test cases
 p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
 p oddities(['abc', 'def']) == ['abc']
 p oddities([123]) == [123]
 p oddities([]) == []
+
+p evens([2, 3, 4, 5, 6]) == [3, 5]
+p evens(['abc', 'def']) == ['def']
+p evens([123]) == []
+p evens([]) == []
