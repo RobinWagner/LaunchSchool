ec6735774f31084952128f931c343b78e71ccd14
Robin Wagner <robin-wagner@web.de>
2017-03-15 07:05:20 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy6.rb
@@ -2,6 +2,13 @@ def xor?(arg1, arg2)
   (arg1 || arg2) && !(arg1 && arg2)
 end
 
+# Alternative solution 1
+def xor?(value1, value2)
+  return true if value1 && !value2
+  return true if value2 && !value1
+  false
+end
+
 # Test cases
 p xor?(5.even?, 4.even?) == true
 p xor?(5.odd?, 4.odd?) == true
