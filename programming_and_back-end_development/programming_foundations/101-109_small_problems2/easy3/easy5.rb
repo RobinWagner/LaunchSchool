03a94525b758162600a75151b4a973e40c6ed369
Robin Wagner <robin-wagner@web.de>
2017-03-15 07:00:29 +0100

    Further exploration added

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy5.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy5.rb
@@ -6,6 +6,14 @@ def square(n)
   multiply(n, n)
 end
 
+def power_to_n(base, exponent)
+  result = base.to_f
+  exponent.to_i.times { result = multiply(base, result) } if exponent >= 0
+  exponent.to_i.abs.times { result = result / base } if exponent < 0
+  result
+end
+
 # Test cases
-p square(5) == 25
-p square(-8) == 64
+# p square(5) == 25
+# p square(-8) == 64
+p power_to_n(3, 3)
