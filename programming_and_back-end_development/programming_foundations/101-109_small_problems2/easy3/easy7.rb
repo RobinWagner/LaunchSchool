10fc5fd7b7b569442b209966a843587017e5eced
Robin Wagner <robin-wagner@web.de>
2017-03-15 07:21:18 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy7.rb
@@ -6,6 +6,15 @@ def oddities(array)
   result
 end
 
+# Alternative 1
+def oddities(array)
+  result = []
+  array.each_with_index do |element, index|
+    result << element if index.even?
+  end
+  result
+end
+
 p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
 p oddities(['abc', 'def']) == ['abc']
 p oddities([123]) == [123]
