31d30288936583e60b3fe377d6664dd92742cfef
Robin Wagner <robin-wagner@web.de>
2017-03-15 07:22:33 +0100

    Add another alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy7.rb
@@ -15,6 +15,17 @@ def oddities(array)
   result
 end
 
+# Alternative 2
+def oddities(array)
+  odd_elements = []
+  index = 0
+  while index < array.size
+    odd_elements << array[index]
+    index += 2
+  end
+  odd_elements
+end
+
 p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
 p oddities(['abc', 'def']) == ['abc']
 p oddities([123]) == [123]
