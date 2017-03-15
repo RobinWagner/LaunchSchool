dcf735c88d415d4c8fb654ecca9b68556690a19d
Robin Wagner <robin-wagner@web.de>
2017-03-15 20:28:52 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy5.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy5.rb
@@ -4,6 +4,21 @@ def multisum(number)
   total
 end
 
+# Alternative solution
+def multiple?(number, divisor)
+  number % divisor == 0
+end
+
+def multisum(max_value)
+  sum = 0
+  1.upto(max_value) do |number|
+    if multiple?(number, 3) || multiple?(number, 5)
+      sum += number
+    end
+  end
+  sum
+end
+
 # Test cases
 p multisum(3) == 3
 p multisum(5) == 8
