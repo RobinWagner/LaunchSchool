4180092a60a2e6a9fabd5910c11bcca2f33b8ea3
Robin Wagner <robin-wagner@web.de>
2017-03-15 12:58:00 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy10.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy3/easy10.rb
@@ -2,6 +2,15 @@ def palindromic_number?(integer)
   integer.to_s.reverse == integer.to_s
 end
 
+# Alternative solution
+def palindromic_number?(number)
+  palindrome?(number.to_s)
+end
+
+def palindrome?(object)
+  object == object.reverse
+end
+
 # Test cases
 p palindromic_number?(34543) == true
 p palindromic_number?(123210) == false
