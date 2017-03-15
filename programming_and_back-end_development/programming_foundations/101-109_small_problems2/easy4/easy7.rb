374c418504c7f3bcc33bc3d1ddbcaf22ce461a54
Robin Wagner <robin-wagner@web.de>
2017-03-15 21:16:03 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy7.rb
@@ -1,10 +1,19 @@
-STRING_TO_NUM = { '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
+DIGITS = { '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
                   '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9}
 
 def string_to_integer(string)
   total = 0
   string.reverse.chars.each_with_index { |char, index| total += STRING_TO_NUM[char] * 10**index }
   total
+end
+
+# Alternative solution
+def string_to_integer(string)
+  digits = string.chars.map { |char| DIGITS[char] }
+
+  value = 0
+  digits.each { |digit| value = 10 * value + digit }
+  value
 end
 
 # Test cases
