42e16112831275503eea50bbfc823f10d89c8362
Robin Wagner <robin-wagner@web.de>
2017-03-16 07:12:50 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy10.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy10.rb
@@ -16,6 +16,15 @@ def integer_to_string(integer)
   string_result.reverse
 end
 
+# Alternative solution
+def signed_integer_to_string(number)
+  case number <=> 0
+  when -1 then "-#{integer_to_string(-number)}"
+  when +1 then "+#{integer_to_string(number)}"
+  else         integer_to_string(number)
+  end
+end
+
 # Test cases
 p signed_integer_to_string(4321) == '+4321'
 p signed_integer_to_string(-123) == '-123'
