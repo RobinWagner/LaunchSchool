2c566476452f196024491a7b71eb3acc718a06b5
Robin Wagner <robin-wagner@web.de>
2017-03-16 06:25:50 +0100

    Add solution for further exploration

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy4/easy7.rb
@@ -16,6 +16,21 @@ def string_to_integer(string)
   value
 end
 
+HEX_DIGITS = { '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5,
+               '6' => 6, '7' => 7, '8' => 8, '9' => 9, 'a' => 10, 'b' => 11,
+               'c' => 12, 'd' => 13, 'e' => 14, 'f' => 15 }
+
+# Further exploration
+def hexadecimal_to_integer(string)
+  total_dec = 0
+  string.downcase.reverse.chars.each_with_index do |char, index|
+    total_dec += HEX_DIGITS[char] * 16**index
+  end
+  total_dec
+end
+
 # Test cases
 p string_to_integer('4321') == 4321
 p string_to_integer('570') == 570
+
+p hexadecimal_to_integer('4D9f') == 19871
