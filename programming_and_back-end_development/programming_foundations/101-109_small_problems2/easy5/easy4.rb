9b1ca2c40133b5a0042bfeb190790d9104075007
Robin Wagner <robin-wagner@web.de>
2017-03-16 20:47:19 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy5/easy4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy5/easy4.rb
@@ -5,6 +5,18 @@ def swap(words)
   words.join(' ')
 end
 
+def swap_first_last_character(word)
+  word[0], word[-1] = word[-1], word[0]
+  word
+end
+
+def swap(words)
+  result = words.split.map do |word|
+    swap_first_last_character(word)
+  end
+  result.join(' ')
+end
+
 # Test cases
 p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
 p swap('Abcde') == 'ebcdA'
