873600863d160d63bd83a27ef31d8352fa2e2c52
Robin Wagner <robin-wagner@web.de>
2017-03-17 07:51:46 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy5/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy5/easy7.rb
@@ -7,6 +7,16 @@ def word_sizes(string)
   result
 end
 
+# Alternative solution
+def word_sizes(words_string)
+  counts = Hash.new(0)
+  words_string.split.each do |word|
+    clean_word = word.delete('^A-Za-z')
+    counts[clean_word.size] += 1
+  end
+  counts
+end
+
 # Test cases
 p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
 p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
