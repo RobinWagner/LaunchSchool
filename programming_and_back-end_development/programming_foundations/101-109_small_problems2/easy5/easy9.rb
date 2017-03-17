dd40d356ceadbc965144356672ad356c71896239
Robin Wagner <robin-wagner@web.de>
2017-03-17 19:21:09 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy5/easy9.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy5/easy9.rb
@@ -2,6 +2,17 @@ def crunch(string)
    string.squeeze
 end
 
+# Alternative solution
+def crunch(text)
+  index = 0
+  crunch_text = ''
+  while index <= text.length - 1
+    crunch_text << text[index] unless text[index] == text[index + 1]
+    index += 1
+  end
+  crunch_text
+end
+
 # Test cases:
 p crunch('ddaaiillyy ddoouubbllee') == 'daily double'
 p crunch('4444abcabccba') == '4abcabcba'
