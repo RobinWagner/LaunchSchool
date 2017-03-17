b7a0c969d2b8b5a1fe8b917bb53576d7af6de8fc
Robin Wagner <robin-wagner@web.de>
2017-03-17 07:34:39 +0100

    Add solution for further exploration

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy5/easy5.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy5/easy5.rb
@@ -10,5 +10,13 @@ def cleanup(text)
   text.gsub(/[^a-z]/i, ' ').squeeze(' ')
 end
 
+# Further exploration (solution without regular expression)
+ALPHABET = ('A'..'Z').to_a + ('a'..'z').to_a
+
+def cleanup(string)
+  result = string.chars.map { |char| ALPHABET.include?(char) ? char : ' ' }
+  result.join.squeeze(' ')
+end
+
 # Test case
 p cleanup("---what's my +*& line?") == ' what s my line '
