cc3643c2c673fb85a64add2ca49a49c351fccda5
Robin Wagner <robin-wagner@web.de>
2017-03-17 19:19:45 +0100

    Exercise 9 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy5/easy9.rb
@@ -0,0 +1,10 @@
+def crunch(string)
+   string.squeeze
+end
+
+# Test cases:
+p crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+p crunch('4444abcabccba') == '4abcabcba'
+p crunch('ggggggggggggggg') == 'g'
+p crunch('a') == 'a'
+p crunch('') == ''
