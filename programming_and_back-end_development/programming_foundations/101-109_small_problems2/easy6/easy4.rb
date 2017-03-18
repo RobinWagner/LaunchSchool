4dfff9ec5d38632a47ee4f0b76224c831c1dc9e3
Robin Wagner <robin-wagner@web.de>
2017-03-18 18:26:38 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy6/easy4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy6/easy4.rb
@@ -6,6 +6,20 @@ def reverse!(array)
   array
 end
 
+# Alternative solution
+def reverse!(array)
+  left_index = 0
+  right_index = -1
+
+  while left_index < array.size / 2
+    array[left_index], array[right_index] = array[right_index], array[left_index]
+    left_index += 1
+    right_index -= 1
+  end
+
+  array
+end
+
 # Test cases
 list = [1,2,3,4]
 result = reverse!(list) # => [4,3,2,1]
