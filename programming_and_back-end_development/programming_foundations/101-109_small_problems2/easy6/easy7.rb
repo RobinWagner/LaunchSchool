9281e89993f6111f687c9735a53dcd2283af6e1f
Robin Wagner <robin-wagner@web.de>
2017-03-18 22:37:41 +0100

    Exercise 7 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy6/easy7.rb
@@ -0,0 +1,19 @@
+def halvsies(array)
+  result = []
+  array1 = []
+  array2 = []
+  array.size.odd? ? half_size = (array.size / 2) : half_size = (array.size / 2) - 1
+  0.upto(half_size) do |index|
+    array1 << array[index]
+  end
+  (half_size + 1).upto(array.size - 1) do |n|
+    array2 << array[n]
+  end
+  result = [array1, array2]
+end
+
+# Test cases
+p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
+p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
+p halvsies([5]) == [[5], []]
+p halvsies([]) == [[], []]
