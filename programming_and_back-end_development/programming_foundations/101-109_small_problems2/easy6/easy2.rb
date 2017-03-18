d2777d823fa703b54fcd0c3299eb32a39a0bd7f9
Robin Wagner <robin-wagner@web.de>
2017-03-18 17:25:34 +0100

    Exercise 2 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy6/easy2.rb
@@ -0,0 +1,15 @@
+VOWELS = ['a', 'e', 'i', 'o', 'u']
+
+def remove_vowels(string_array)
+  result = []
+  string_array.each do |string|
+    string_new = string.chars.select { |char| !VOWELS.include?(char.downcase) }
+    result << string_new.join('')
+  end
+  result
+end
+
+# Test cases
+p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
