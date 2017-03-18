f1b5b48a15c444650bb507363e598b931a02bbfa
Robin Wagner <robin-wagner@web.de>
2017-03-18 18:43:15 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy6/easy6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy6/easy6.rb
@@ -2,5 +2,14 @@ def merge(array1, array2)
   array1.zip(array2).flatten.uniq.sort
 end
 
+# Alternative solution
+def merger(array1, array2)
+  new_array = array1
+  while array2.to_s.size > 0
+    new_array << array2.pop
+  end
+  new_array.flatten.uniq.sort
+end
+
 # Test case
 p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
