00b088d01fefedb4bcc5a1ca4e6155cb2919c51e
Robin Wagner <robin-wagner@web.de>
2017-03-18 22:38:57 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy6/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy6/easy7.rb
@@ -12,6 +12,13 @@ def halvsies(array)
   result = [array1, array2]
 end
 
+# Alternative solution
+def halvsies(array)
+  first_half = array.slice(0, (array.size / 2.0).ceil)
+  second_half = array.slice(first_half.size, array.size - first_half.size)
+  [first_half, second_half]
+end
+
 # Test cases
 p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
 p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
