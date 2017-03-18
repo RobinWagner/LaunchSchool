773b799327b9b7d59f8f008158c203eca6c3f18d
Robin Wagner <robin-wagner@web.de>
2017-03-18 18:24:00 +0100

    Exercise 4 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy6/easy4.rb
@@ -0,0 +1,25 @@
+def reverse!(array)
+  half_size = (array.size / 2) - 1
+  0.upto(half_size) do |index|
+    array[index], array[array.size-(index+1)] = array[array.size-(index+1)], array[index]
+  end
+  array
+end
+
+# Test cases
+list = [1,2,3,4]
+result = reverse!(list) # => [4,3,2,1]
+p list == [4, 3, 2, 1]
+list.object_id == result.object_id
+
+list = %w(a b c d e)
+reverse!(list) # => ["e", "d", "c", "b", "a"]
+p list == ["e", "d", "c", "b", "a"]
+
+list = ['abc']
+reverse!(list) # => ["abc"]
+p list == ["abc"]
+
+list = []
+reverse!([]) # => []
+p list == []
