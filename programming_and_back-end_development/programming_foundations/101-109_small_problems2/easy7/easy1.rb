f5085142ed7c88aaa107b5bd3cccfadcda77fb5a
Robin Wagner <robin-wagner@web.de>
2017-03-19 08:40:00 +0100

    Add another alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy1.rb
@@ -12,5 +12,14 @@ def interleave(array1, array2)
   result
 end
 
+# Alternative solution 2
+def interleave(array1, array2)
+  result = []
+  array1.each_with_index do |element, index|
+    result << element << array2[index]
+  end
+  result
+end
+
 # Test case
 p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
