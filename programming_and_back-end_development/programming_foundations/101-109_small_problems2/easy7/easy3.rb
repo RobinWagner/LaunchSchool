33da787617d5551b8a4d753b462a5bd35131e388
Robin Wagner <robin-wagner@web.de>
2017-03-19 10:05:21 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy3.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy3.rb
@@ -2,6 +2,14 @@ def word_cap(sentence)
   sentence.split(' ').map { |word| word.capitalize }.join(' ')
 end
 
+# Alternative solution
+def word_cap(words)
+  words_array = words.split.map do |word|
+    word.capitalize
+  end
+  words_array.join(' ')
+end
+
 # Test cases
 p word_cap('four score and seven') == 'Four Score And Seven'
 p word_cap('the javaScript language') == 'The Javascript Language'
