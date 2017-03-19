43d4a6333165cb9c712c96e6c55d5bd3892a8c78
Robin Wagner <robin-wagner@web.de>
2017-03-19 08:07:59 +0100

    Exercise 9 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy6/easy9.rb
@@ -0,0 +1,13 @@
+def include?(array, number)
+  0.upto(array.size - 1) do |index|
+    return true if array[index] == number
+  end
+  false
+end
+
+# Test cases
+p include?([1,2,3,4,5], 3) == true
+p include?([1,2,3,4,5], 6) == false
+p include?([], 3) == false
+p include?([nil], nil) == true
+p include?([], nil) == false
