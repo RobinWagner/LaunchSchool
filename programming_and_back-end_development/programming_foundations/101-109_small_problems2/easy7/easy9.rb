73aac3c46c160b7c1ef9fee396be3d41e899b28d
Robin Wagner <robin-wagner@web.de>
2017-03-20 13:06:26 +0100

    Exercise 9 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy9.rb
@@ -0,0 +1,12 @@
+def multiply_all_pairs(array1, array2)
+  result = []
+  array1.each do |element|
+    array2.each do |number|
+      result << element * number
+    end
+  end
+  result.sort
+end
+
+# Test case
+p multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
