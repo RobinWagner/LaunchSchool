011805b18e742e51532a0181405c50f4b6d4b50e
Robin Wagner <robin-wagner@web.de>
2017-03-20 19:44:43 +0100

    Add solution for exercise 1

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy8/easy1.rb
@@ -0,0 +1,14 @@
+def sum_of_sums(array)
+  sum = 0
+  while array.size > 0
+    sum += array.inject(:+)
+    array.pop
+  end
+  sum
+end
+
+# Test cases
+p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums([4]) == 4
+p sum_of_sums([1, 2, 3, 4, 5]) == 35
