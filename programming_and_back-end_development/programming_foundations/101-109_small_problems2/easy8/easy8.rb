20b9efe88158f7a99770c2007bfb0f0d09b58b75
Robin Wagner <robin-wagner@web.de>
2017-03-20 21:57:34 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy8/easy8.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy8/easy8.rb
@@ -9,6 +9,18 @@ def double_consonants(string)
   result.join
 end
 
+# Alternative solution
+CONSONANTS = %w(b c d f g h j k l m n p q r s t v w x y z)
+
+def double_consonants(string)
+  result = ''
+  string.each_char do |char|
+    result << char
+    result << char if CONSONANTS.include?(char.downcase)
+  end
+  result
+end
+
 # Test cases
 p double_consonants('String') == "SSttrrinngg"
 p double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
