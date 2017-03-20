a819fc0f8cf93cc9dc462a70462f236230af24e2
Robin Wagner <robin-wagner@web.de>
2017-03-20 19:46:13 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy8/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy8/easy1.rb
@@ -7,6 +7,15 @@ def sum_of_sums(array)
   sum
 end
 
+# Alternative solution
+def sum_of_sums(numbers)
+  sum_total = 0
+  1.upto(numbers.size) do |count|
+    sum_total += numbers.slice(0, count).reduce(:+)
+  end
+  sum_total
+end
+
 # Test cases
 p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
 p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
