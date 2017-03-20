f983fbf8cf1c6bbde20d2c4f596ee41b7536c11d
Robin Wagner <robin-wagner@web.de>
2017-03-20 21:26:06 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy8/easy5.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy8/easy5.rb
@@ -18,6 +18,20 @@ def substrings_at_start(string)
   result
 end
 
+# Alternative solution
+def palindromes(string)
+  all_substrings = substrings(string)
+  results = []
+  all_substrings.each do |substring|
+    results << substring if palindrome?(substring)
+  end
+  results
+end
+
+def palindrome?(string)
+  string == string.reverse && string.size > 1
+end
+
 # Test cases
 p palindromes('abcd') == []
 p palindromes('madam') == ['madam', 'ada']
