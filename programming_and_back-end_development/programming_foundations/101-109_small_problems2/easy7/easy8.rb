ff1db5f4efa7b003cb20cd23fd43116082581b26
Robin Wagner <robin-wagner@web.de>
2017-03-20 13:03:47 +0100

    Add solution for further exploration

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy8.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy8.rb
@@ -6,5 +6,10 @@ def multiply_list(array1, array2)
   result
 end
 
+# Further exploration
+def multiply_list(array1, array2)
+  array1.zip(array2).map { |e| e[0] * e[1] }
+end
+
 # Test case
 p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
