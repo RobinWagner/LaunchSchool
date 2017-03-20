cb4d740094f0506caf3fdb4d5ab7b0fa52f0d7f9
Robin Wagner <robin-wagner@web.de>
2017-03-20 12:42:51 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy7.rb
@@ -3,6 +3,14 @@ def show_multiplicative_average(array)
   puts "The result is #{format("%.3f", result)}"
 end
 
+# Alternative solution
+def show_multiplicative_average(numbers)
+  product = 1.to_f
+  numbers.each { |number| product *= number }
+  average = product / numbers.size
+  puts format('%.3f', average)
+end
+
 # Test cases
 show_multiplicative_average([3, 5])
 show_multiplicative_average([2, 5, 7, 11, 13, 17])
