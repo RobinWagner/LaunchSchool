d1155e7c7bed01867afa75fdf9fc55eaecce8dc5
Robin Wagner <robin-wagner@web.de>
2017-03-20 19:40:19 +0100

    Exercise 10 completed

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy10.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy7/easy10.rb
@@ -8,6 +8,23 @@ def penultimate(words)
   words_array[-2]
 end
 
+# Further exploration
+def middle_word(words)
+  return '' if words.size == 0
+  if words.split.size.odd?
+    return words.split[0] if words.size == 1
+    return words.split[words.split.size / 2]
+  else
+    words.split[(words.split.size / 2 - 1)..(words.split.size / 2)].join(' ')
+  end
+end
+
 # Test cases
-p penultimate('last word') == 'last'
-p penultimate('Launch School is great!') == 'is'
+# p penultimate('last word') == 'last'
+# p penultimate('Launch School is great!') == 'is'
+
+p middle_word('These are words') == 'are'
+p middle_word('') == ''
+p middle_word('one') == 'one'
+p middle_word('two words') == 'two words'
+p middle_word('These are four words') == 'are four'
