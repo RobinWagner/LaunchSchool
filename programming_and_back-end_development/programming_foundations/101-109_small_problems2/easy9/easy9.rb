80dff48b296222340197a40059b4b6775c60d75e
Robin Wagner <robin-wagner@web.de>
2017-03-21 19:46:21 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy9/easy9.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy9/easy9.rb
@@ -14,6 +14,19 @@ def get_grade(score1, score2, score3)
   end
 end
 
+# Alternative solution
+def get_grade(s1, s2, s3)
+  result = (s1 + s2 + s3)/3
+
+  case result
+  when 90..100 then 'A'
+  when 80..89  then 'B'
+  when 70..79  then 'C'
+  when 60..69  then 'D'
+  else              'F'
+  end
+end
+
 # Test cases
 p get_grade(95, 90, 93) == "A"
 p get_grade(50, 50, 95) == "D"
