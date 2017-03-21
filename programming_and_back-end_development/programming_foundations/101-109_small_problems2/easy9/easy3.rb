a67048339299603c28b54067e6a233dafeab329d
Robin Wagner <robin-wagner@web.de>
2017-03-21 08:02:16 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy9/easy3.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy9/easy3.rb
@@ -2,6 +2,11 @@ def negative(number)
   number.abs * -1
 end
 
+# Alternative solution
+def negative(number)
+  number > 0 ? number * -1 : number
+end
+
 # Test cases
 p negative(5) == -5
 p negative(-3) == -3
