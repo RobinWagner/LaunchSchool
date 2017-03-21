64e6377150ff3976ae6b85a8ccbf0f3aea1b3fd9
Robin Wagner <robin-wagner@web.de>
2017-03-21 07:42:24 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy9/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy9/easy1.rb
@@ -1,8 +1,14 @@
 def greetings(array, hash)
   name = array.join(' ')
-  puts "=> Hello, #{name}! " \
-       "Nice to have a #{hash[:title]} #{hash[:occupation]} around."
+  "=> Hello, #{name}! " \
+  "Nice to have a #{hash[:title]} #{hash[:occupation]} around."
+end
+
+# Alternative solution
+def greetings(name, status)
+  "Hello, #{name.join(' ')}! Nice to have a " \
+  "#{status[:title]} #{status[:occupation]} around."
 end
 
 # Test case
-greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
+puts greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
