bde83305be9614de421a06efb2495f8cf880bf58
Robin Wagner <robin-wagner@web.de>
2017-03-21 19:56:02 +0100

    Exercise 10 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy9/easy10.rb
@@ -0,0 +1,11 @@
+def buy_fruit(list)
+  result = []
+  list.each do |elements|
+    elements[1].times { result << elements[0] }
+  end
+  result
+end
+
+# Test case
+p buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) ==
+  ["apples", "apples", "apples", "orange", "bananas","bananas"]
