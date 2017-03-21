ac3494c746fc1ff738dafa2b4dba0c180c7f16af
Robin Wagner <robin-wagner@web.de>
2017-03-21 12:32:14 +0100

    Add solution for further exploration

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy9/easy4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/easy9/easy4.rb
@@ -11,7 +11,21 @@ def sequence(number)
   (1..number).to_a
 end
 
+# Further exploration
+def sequence_adv(integer)
+  if integer > 0
+    1.upto(integer).to_a
+  elsif integer == 0
+    [0]
+  else
+    1.downto(integer).to_a
+  end
+end
+
+
 # Test cases
 p sequence(5) == [1, 2, 3, 4, 5]
 p sequence(3) == [1, 2, 3]
 p sequence(1) == [1]
+p sequence_adv(0) == [0]
+p sequence_adv(-3) == [1, 0, -1, -2, -3]
