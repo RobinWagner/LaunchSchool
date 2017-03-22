1fcc47ecacf0b87b025d142ffb1d0980004c696e
Robin Wagner <robin-wagner@web.de>
2017-03-22 12:45:50 +0100

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium2.rb
@@ -8,6 +8,13 @@ def rotate_array(array)
   array[1..-1] + [array[0]]
 end
 
+# Alternative solution
+def rotate_rightmost_digits(number, n)
+  all_digits = number.to_s.chars
+  all_digits[-n..-1] = rotate_array(all_digits[-n..-1])
+  all_digits.join.to_i
+end
+
 # Test cases
 p rotate_rightmost_digits(735291, 1) == 735291
 p rotate_rightmost_digits(735291, 2) == 735219
