0ebb81d8b15ec276055ead3c9e87277ddaf0db54
Robin Wagner <robin-wagner@web.de>
2017-03-26 20:12:52 +0200

    Exercise 9 completed

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium9.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium9.rb
@@ -1,35 +1,17 @@
 def fibonacci(n)
-  return 1 if n == 1
-  return 1 if n == 2
-  fibonacci(n - 1) + fibonacci(n - 2)
-end
-
-# Alternative solution
-def fibonacci(nth)
-  return 1 if nth <= 2
-  fibonacci(nth - 1) + fibonacci(nth - 2)
-end
-
-# Optimized solution (tail recursion)
-def fibonacci_tail(nth, acc1, acc2)
-  if nth == 1
-    acc1
-  elsif nth == 2
-    acc2
-  else
-    fibonacci_tail(nth - 1, acc2, acc2 + acc1)
+  return 1 if n <= 2
+  total = 0
+  num1 = 1
+  num2 = 0
+  1.upto(n) do |num|
+    total = num1 + num2
+    num1 = num2
+    num2 = total
   end
-end
-
-def fibonacci(nth)
-  fibonacci_tail(nth, 1, 1)
+  total
 end
 
 # Test cases
-p fibonacci(1) == 1
-p fibonacci(2) == 1
-p fibonacci(3) == 2
-p fibonacci(4) == 3
-p fibonacci(5) == 5
-p fibonacci(12) == 144
 p fibonacci(20) == 6765
+p fibonacci(100) == 354224848179261915075
+p fibonacci(100_001) # 4202692702.....8285979669707537501
