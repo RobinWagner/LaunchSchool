541c4c5cb6e4097bc57508436cfc896ad7551ccf
Robin Wagner <robin-wagner@web.de>
2017-03-26 20:07:34 +0200

    Add optimized solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium9.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium9.rb
@@ -10,6 +10,21 @@ def fibonacci(nth)
   fibonacci(nth - 1) + fibonacci(nth - 2)
 end
 
+# Optimized solution (tail recursion)
+def fibonacci_tail(nth, acc1, acc2)
+  if nth == 1
+    acc1
+  elsif nth == 2
+    acc2
+  else
+    fibonacci_tail(nth - 1, acc2, acc2 + acc1)
+  end
+end
+
+def fibonacci(nth)
+  fibonacci_tail(nth, 1, 1)
+end
+
 # Test cases
 p fibonacci(1) == 1
 p fibonacci(2) == 1
