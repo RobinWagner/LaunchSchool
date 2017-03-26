56913485749f89d13def66348a3c8230ef97a795
Robin Wagner <robin-wagner@web.de>
2017-03-26 20:08:39 +0200

    Assign to right exercise number

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium8.rb
@@ -0,0 +1,35 @@
+def fibonacci(n)
+  return 1 if n == 1
+  return 1 if n == 2
+  fibonacci(n - 1) + fibonacci(n - 2)
+end
+
+# Alternative solution
+def fibonacci(nth)
+  return 1 if nth <= 2
+  fibonacci(nth - 1) + fibonacci(nth - 2)
+end
+
+# Optimized solution (tail recursion)
+def fibonacci_tail(nth, acc1, acc2)
+  if nth == 1
+    acc1
+  elsif nth == 2
+    acc2
+  else
+    fibonacci_tail(nth - 1, acc2, acc2 + acc1)
+  end
+end
+
+def fibonacci(nth)
+  fibonacci_tail(nth, 1, 1)
+end
+
+# Test cases
+p fibonacci(1) == 1
+p fibonacci(2) == 1
+p fibonacci(3) == 2
+p fibonacci(4) == 3
+p fibonacci(5) == 5
+p fibonacci(12) == 144
+p fibonacci(20) == 6765
