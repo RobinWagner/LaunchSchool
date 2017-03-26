533e93dc8420d130b5c624a093f379b78807c389
Robin Wagner <robin-wagner@web.de>
2017-03-26 20:26:49 +0200

    Add optimized solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium10.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium10.rb
@@ -15,6 +15,16 @@ def fibonacci_last(nth)
   fibonacci(nth).to_s[-1].to_i
 end
 
+# Optimized solution
+def fibonacci_last(nth)
+  last_2 = [1, 1]
+  3.upto(nth) do
+    last_2 = [last_2.last, (last_2.first + last_2.last) % 10]
+  end
+  
+  last_2.last
+end
+
 # Test cases
 p fibonacci_last(15)        == 0        # -> 0  (the 15th Fibonacci number is 610)
 p fibonacci_last(20)        == 5       # -> 5 (the 20th Fibonacci number is 6765)
