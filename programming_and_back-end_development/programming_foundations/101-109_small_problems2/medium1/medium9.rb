ae22a0d49a250fcf339924dee66deff6d3b5cf1b
Robin Wagner <robin-wagner@web.de>
2017-03-26 20:15:25 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium9.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium1/medium9.rb
@@ -11,6 +11,15 @@ def fibonacci(n)
   total
 end
 
+# Alternative solution
+def fibonacci(nth)
+  first, last = [1, 1]
+  3.upto(nth) do
+    first, last = [last, first + last]
+  end
+  last
+end
+
 # Test cases
 p fibonacci(20) == 6765
 p fibonacci(100) == 354224848179261915075
