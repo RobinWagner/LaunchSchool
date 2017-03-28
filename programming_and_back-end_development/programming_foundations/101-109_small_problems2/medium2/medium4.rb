abcb3004a0e355e541a0274429ffd9591410c769
Robin Wagner <robin-wagner@web.de>
2017-03-28 18:17:32 +0200

    Add solution to further exploration

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium4.rb
@@ -20,6 +20,18 @@ def balanced?(string)
   parens.zero?
 end
 
+# Further exploration
+def balanced_characters(string, char_open, char_close)
+  character_count = 0
+  string.each_char do |char|
+    character_count += 1 if char == char_open
+    character_count -= 1 if char == char_close
+    break if character_count < 0
+  end
+
+  character_count.zero?
+end
+
 # Test cases
 p balanced?('What (is) this?') == true
 p balanced?('What is) this?') == false
@@ -29,3 +41,6 @@ p balanced?('((What)) (is this))?') == false
 p balanced?('Hey!') == true
 p balanced?(')Hey!(') == false
 p balanced?('What ((is))) up(') == false
+
+p balanced_characters('What (is) this?', '(', ')') == true
+p balanced_characters('What {is} this?', '{', '}') == true
