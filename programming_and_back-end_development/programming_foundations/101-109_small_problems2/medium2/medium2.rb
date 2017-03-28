a394423ac9cd97c136716e5ecaa7658cad1a48ab
Robin Wagner <robin-wagner@web.de>
2017-03-28 12:18:59 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium2.rb
@@ -21,6 +21,14 @@ def block_word?(word)
   true
 end
 
+# Alternative solution
+BLOCKS = %w(BO XK DQ CP NA GT RE FS JW HU VI LY ZM).freeze
+
+def block_word?(string)
+  up_string = string.upcase
+  BLOCKS.none? { |block| up_string.count(block) >= 2 }
+end
+
 # Test cases
 p block_word?('BATCH') == true
 p block_word?('BUTCH') == false
