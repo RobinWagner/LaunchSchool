a58dd0a0941de33ff273b98038de0801849434d9
Robin Wagner <robin-wagner@web.de>
2017-03-28 18:34:42 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium5.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium5.rb
@@ -8,6 +8,23 @@ def triangle(side1, side2, side3)
   end
 end
 
+# Alternative solution
+def triangle(side1, side2, side3)
+  sides = [side1, side2, side3]
+  largest_side = sides.max
+
+  case
+  when 2 * largest_side > sides.reduce(:+), sides.include?(0)
+    :invalid
+  when side1 == side2 && side2 == side3
+    :equilateral
+  when side1 == side2 || side2 == side3 || side2 == side3
+    :isosceles
+  else
+    :scalene
+  end
+end
+
 # Test cases
 p triangle(3, 3, 3) == :equilateral
 p triangle(3, 3, 1.5) == :isosceles
