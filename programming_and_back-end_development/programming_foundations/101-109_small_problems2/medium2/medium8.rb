d3935884ab720bbdfe3520f26547274cd4c5dd97
Robin Wagner <robin-wagner@web.de>
2017-03-29 12:32:21 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium8.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium8.rb
@@ -7,6 +7,21 @@ def featured(start)
   end
 end
 
+# Alternative solution
+def featured(number)
+  number += 1
+  number += 1 until number.odd? && number % 7 == 0
+
+  loop do
+    number_chars = number.to_s.split('')
+    return number if number_chars.uniq == number_chars
+    number += 14
+    break if number >= 9_876_543_210
+  end
+
+  'There is no possible number that fulfills those requirements.'
+end
+
 # Test cases
 p featured(12) == 21
 p featured(20) == 21
