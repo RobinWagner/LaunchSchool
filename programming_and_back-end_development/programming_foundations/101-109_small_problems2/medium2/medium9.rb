b006dcedc85f747e2cec341a9e34348c1ea794d8
Robin Wagner <robin-wagner@web.de>
2017-03-29 21:19:29 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium9.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium9.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def bubble_sort!(array)
   (array.size - 1).times do
     0.upto(array.size - 2) do |i|
@@ -9,6 +7,21 @@ def bubble_sort!(array)
     end
   end
   array
+end
+
+# Alternative solution
+def bubble_sort!(array)
+  loop do
+    swapped = false
+    1.upto(array.size - 1) do |index|
+      next if arrah[index - 1] <= array[index]
+      array[index - 1], array[index] = array[index], array[index - 1]
+      swapped = true
+    end
+
+    break unless swapped
+  end
+  nil
 end
 
 # Test cases
