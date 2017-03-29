e88ecb17f15b9691f70e5407daf3d105bdc10c06
Robin Wagner <robin-wagner@web.de>
2017-03-29 21:37:04 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium10.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/medium2/medium10.rb
@@ -2,6 +2,19 @@ def sum_square_difference(number)
   (1..number).reduce(:+)**2 - (1..number).map { |n| n** 2 }.reduce(:+)
 end
 
+# Alternative solution
+def sum_square_difference(n)
+  sum = 0
+  sum_of_squares = 0
+
+  1.upto(n) do |value|
+    sum += value
+    sum_of_squares += value**2
+  end
+
+  sum**2 - sum_of_squares
+end
+
 # Test cases
 p sum_square_difference(3) == 22
    # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
