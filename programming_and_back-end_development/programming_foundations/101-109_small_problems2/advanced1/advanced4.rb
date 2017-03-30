16c9189da361033ad6a558453bd8567d683eee3e
Robin Wagner <robin-wagner@web.de>
2017-03-30 18:40:02 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems2/advanced1/advanced4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/advanced1/advanced4.rb
@@ -7,6 +7,18 @@ def transpose(matrix)
   result
 end
 
+# Alternative solution
+def transpose(matrix)
+  result = []
+  number_of_rows = matrix.size
+  number_of_columns = matrix.first.size
+  (0...number_of_columns).each do |column_index|
+    new_row = (0...number_of_rows).map { |row_index| matrix[row_index][column_index] }
+    result << new_row
+  end
+  result
+end
+
 # Test cases
 p transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
 p transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
