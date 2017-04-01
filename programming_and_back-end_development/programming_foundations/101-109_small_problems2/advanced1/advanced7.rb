f7e0e702a227c910acab0af11f5cac4447483e51
Robin Wagner <robin-wagner@web.de>
2017-04-01 13:43:30 +0200

    Exercise 7 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems2/advanced1/advanced7.rb
@@ -0,0 +1,20 @@
+def merge (array1, array2)
+  index2 = 0
+  result = []
+
+  array1.each do |value|
+    while index2 < array2.size && array2[index2] <= value
+      result << array2[index2]
+      index2 += 1
+    end
+    result << value
+  end
+
+  result.concat(array2[index2..-1])
+end
+
+# Test cases
+p merge([1, 5, 9], [2, 6, 8]) == [1, 2, 5, 6, 8, 9]
+p merge([1, 1, 3], [2, 2]) == [1, 1, 2, 2, 3]
+p merge([], [1, 4, 5]) == [1, 4, 5]
+p merge([1, 4, 5], []) == [1, 4, 5]
