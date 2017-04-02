c82084ac6d6fe881bf5a9097cabfe260a66dde5b
Robin Wagner <robin-wagner@web.de>
2017-04-02 09:27:44 +0200

    Refactor solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy4/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy4/easy7.rb
@@ -4,8 +4,9 @@ STRING_TO_NUM = { '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
 
 def string_to_integer(string)
   result = 0
-  num_array = string.chars.map { |char| STRING_TO_NUM[char] }
-  num_array.reverse.each_with_index { |num, i| result += num * 10**i }
+  num_array = string.chars.reverse.each_with_index.map do
+    |char, i| result += STRING_TO_NUM[char] * 10**i
+  end
   result
 end
 
