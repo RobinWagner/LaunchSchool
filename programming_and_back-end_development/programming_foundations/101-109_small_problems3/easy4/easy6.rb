cdb04c80f0b5c899d6b03fa5de852ce8be4e6cbe
Robin Wagner <robin-wagner@web.de>
2017-04-02 09:17:26 +0200

    Add solution for further exploration

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy4/easy6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy4/easy6.rb
@@ -14,6 +14,12 @@ def running_total(array)
   array.map { |value| sum += value }
 end
 
+# Further exploration
+def running_total(array)
+  sum = 0
+  array.each_with_object([]) { |num, total| total << num + total.last.to_i }
+end
+
 # Test cases
 p running_total([2, 5, 13]) == [2, 7, 20]
 p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
