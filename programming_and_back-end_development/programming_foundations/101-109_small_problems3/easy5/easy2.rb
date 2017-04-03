29b6059d66cafba64d7958dda23d0182860fea90
Robin Wagner <robin-wagner@web.de>
2017-04-03 08:14:52 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy5/easy2.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy5/easy2.rb
@@ -1,6 +1,6 @@
 MINUTES_PER_HOUR = 60
 HOURS_PER_DAY = 24
-MINUTS_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
 
 def time_of_day(minutes)
   hours = minutes / MINUTES_PER_HOUR
@@ -8,6 +8,13 @@ def time_of_day(minutes)
   minutes = minutes % 60
 
   "#{sprintf("%02d", hours)}:#{sprintf("%02d", minutes)}"
+end
+
+# Alternative solution
+def time_of_day(delta_minutes)
+  delta_minutes = delta_minutes % MINUTES_PER_DAY
+  hours, minutes = delta_minutes.divmod(MINUTES_PER_HOUR)
+  format('%02d:%02d', hours, minutes)
 end
 
 # Test cases
