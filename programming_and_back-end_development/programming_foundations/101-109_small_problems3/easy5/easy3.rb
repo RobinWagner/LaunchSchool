695bb30aad45f57f6fad27318713b34886245c1c
Robin Wagner <robin-wagner@web.de>
2017-04-08 12:55:49 +0200

    Exercise 3 completed

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy5/easy3.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy5/easy3.rb
@@ -3,6 +3,17 @@ HOURS_PER_DAY = 24
 MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
 
 def after_midnight(time)
+  time_array = time.split(':')
+  hours = time_array[0].to_i
+  return 0 if hours == 0 || hours == 24
+  minutes = time_array[1].to_i + hours * MINUTES_PER_HOUR
+end
+
+def before_midnight(time)
+  time_array = time.split(':')
+  hours = time_array[0].to_i
+  return 0 if hours == 0 || hours == 24
+  minutes = MINUTES_PER_DAY - (time_array[1].to_i + hours * MINUTES_PER_HOUR)
 end
 
 # Test cases
