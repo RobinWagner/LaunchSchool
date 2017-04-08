9f5d9916e5d766de45d12f0ee2a94ca6e895336c
Robin Wagner <robin-wagner@web.de>
2017-04-08 13:08:18 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy5/easy5.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy5/easy5.rb
@@ -2,5 +2,10 @@ def cleanup(string)
   string.gsub(/[^a-z]/i, ' ').squeeze
 end
 
+# Alternative solution
+def cleanup(text)
+  text.gsub(/[^a-z]/i, ' ').squeeze(' ')
+end
+
 # Test case
 p cleanup("---what's my +*& line?") == ' what s my line '
