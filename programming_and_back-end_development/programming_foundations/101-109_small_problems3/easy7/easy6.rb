5284c81da0133cbe1dd7b96cc6515f9e59de247b
Robin Wagner <robin-wagner@web.de>
2017-04-13 12:24:28 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy7/easy6.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy7/easy6.rb
@@ -15,6 +15,25 @@ def staggered_case(string)
   new_string_arr.join
 end
 
+# Alternative solution
+def staggered_case(string)
+  result = ''
+  need_upper = true
+  string.chars.each do |char|
+    if char =~ /[a-z]/i
+      if need_upper
+        result += char.upcase
+      else
+        result += char.downcase
+      end
+      need_upper = !need_upper
+    else
+      result += char
+    end
+  end
+  result
+end
+
 # Test cases
 p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
 p staggered_case('ALL CAPS') == 'AlL cApS'
