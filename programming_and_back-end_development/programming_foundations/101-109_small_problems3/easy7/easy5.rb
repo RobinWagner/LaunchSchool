051327e3b2898810047ab48db6013f1ae8d8f798
Robin Wagner <robin-wagner@web.de>
2017-04-13 07:56:24 +0200

    Exercise 5 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy7/easy5.rb
@@ -0,0 +1,14 @@
+def staggered_case(string)
+  index = 0
+  new_string_arr = []
+  string.chars.each do |n|
+    index.even? ? (new_string_arr << n.upcase) : (new_string_arr << n.downcase)
+    index += 1
+  end
+  new_string_arr.join
+end
+
+# Test cases
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
