82f0ab5f54f3112868c955becc71f2685cc1f426
Robin Wagner <robin-wagner@web.de>
2017-04-13 07:42:38 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy6/easy10.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/easy6/easy10.rb
@@ -4,6 +4,18 @@ def triangle(n)
   end
 end
 
+# Alternative solution
+def triangle(num)
+  spaces = num - 1
+  stars = 1
+
+  num.times do |n|
+    puts (' ' * spaces) + ('*' * stars)
+    spaces -= 1
+    stars += 1
+  end
+end
+
 # Test cases
 triangle(5)
 triangle(9)
