0eec6ac8955e292d43ace6e75b2107700eade6f0
Robin Wagner <robin-wagner@web.de>
2017-04-14 15:27:51 +0200

    Add missing hash value

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy7.rb
@@ -1,5 +1,6 @@
-WORD_DIGIT = { 'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5,
-               'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9, 'ten' => 10 }
+WORD_DIGIT = { 'zero' => 0, 'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4,
+               'five' => 5, 'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9,
+               'ten' => 10 }
 
 def word_to_digit(string)
   result = []
@@ -11,4 +12,4 @@ def word_to_digit(string)
 end
 
 # Test case
-p word_to_digit('Please call me at five five five one two three four. Thanks.')# == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
+p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
