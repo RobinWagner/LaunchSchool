5c5b27d3c1c2d03316c5e7562cc3a12e10bd16f5
Robin Wagner <robin-wagner@web.de>
2017-04-14 02:11:54 +0200

    Add solution for rotating integers

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy1.rb
@@ -16,6 +16,10 @@ def rotate_string(string)
   rotate_array(string.chars).join('')
 end
 
+def rotate_integers(int)
+  rotate_array(int.to_s.chars).join('').to_i
+end
+
 # Test cases
 p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
 p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
@@ -27,3 +31,6 @@ p x == [1, 2, 3, 4]                 # => true
 
 p rotate_string('hello') == 'elloh'
 p rotate_string('o') == 'o'
+
+p rotate_integers(123) == 231
+p rotate_integers(2) == 2
