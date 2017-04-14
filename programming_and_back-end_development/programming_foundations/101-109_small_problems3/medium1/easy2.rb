a8a0f6e379c3b01e7b58ec542754fd1c7924f3aa
Robin Wagner <robin-wagner@web.de>
2017-04-14 09:21:27 +0200

    Exercise 2 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy2.rb
@@ -0,0 +1,18 @@
+def rotate_rightmost_digits(number, n)
+  num_array = number.to_s.chars
+  leading_part = num_array[0,(number.to_s.size - n)].join('')
+  rotated_part = rotate_array(num_array[-n..-1]).join('')
+  (leading_part + rotated_part).to_i
+end
+
+def rotate_array(array)
+  array[1..-1] + [array[0]]
+end
+
+# Test cases
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
+p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
