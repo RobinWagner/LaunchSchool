d43a32a8540ea04da090d4915bdf03520e99e982
Robin Wagner <robin-wagner@web.de>
2017-04-14 15:31:11 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy7.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy7.rb
@@ -1,14 +1,22 @@
-WORD_DIGIT = { 'zero' => 0, 'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4,
-               'five' => 5, 'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9,
-               'ten' => 10 }
+WORD_DIGIT = { 'zero' => '0', 'one' => '1', 'two' => '2', 'three' => '3',
+               'four' => '4', 'five' => '5', 'six' => '6', 'seven' => '7',
+               'eight' => '8', 'nine' => '9', 'ten' => '10' }
 
 def word_to_digit(string)
   result = []
   string.split.each do |word|
-    WORD_DIGIT.keys.each { |key| word.gsub!(key, WORD_DIGIT[key].to_s) if word.include?(key) }
+    WORD_DIGIT.keys.each { |key| word.gsub!(key, WORD_DIGIT[key]) if word.include?(key) }
     result << word
   end
   result.join(' ')
+end
+
+# Alternative solution
+def word_to_digit(words)
+  WORD_DIGIT.keys.each do |word|
+    words.gsub!(/\b#{word}\b/, WORD_DIGIT[word])
+  end
+  words
 end
 
 # Test case
