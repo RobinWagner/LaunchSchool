5134f6cc8961d951fd539a18c5c5c942a8f7c966
Robin Wagner <robin-wagner@web.de>
2017-04-14 10:46:30 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy4.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy4.rb
@@ -1,4 +1,3 @@
-
 def switch_lights(lights_num)
   lights = (1..lights_num).to_a.zip(['on'] * lights_num).to_h
   sequence = 2
@@ -12,5 +11,25 @@ def switch_lights(lights_num)
   lights_on = lights.select { |key, value| value == 'on' }.keys
   puts "These lights are on: #{lights_on}."
 end
+
+# Alternative solution
+def toggle_on_or_off(lights)
+  1.upto(lights.size) do |round_number|
+    lights.each do |position, value|
+      if position % round_number == 0
+        value == 'off' ? lights[position] = 'on' : lights[position] = 'off'
+      end
+    end
+  end
+end
+
+def on_lights(lights)
+  lights.keys.select { |key| lights[key] == 'on' }
+end
+
+lights = Hash.new
+1.upto(1000) { |number| lights[number] = 'off' }
+toggle_on_or_off(lights)
+p on_lights(lights)
 
 switch_lights(1000)
