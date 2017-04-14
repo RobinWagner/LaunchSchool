896981c27a093d1b9ee0417517c957f500e76650
Robin Wagner <robin-wagner@web.de>
2017-04-14 02:09:38 +0200

    Add solution for rotating strings

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy1.rb
@@ -11,6 +11,11 @@ def rotate_array(array)
   array[1..-1] + [array[0]]
 end
 
+# Further exploration
+def rotate_string(string)
+  rotate_array(string.chars).join('')
+end
+
 # Test cases
 p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
 p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
@@ -19,3 +24,6 @@ p rotate_array(['a']) == ['a']
 x = [1, 2, 3, 4]
 p rotate_array(x) == [2, 3, 4, 1]   # => true
 p x == [1, 2, 3, 4]                 # => true
+
+p rotate_string('hello') == 'elloh'
+p rotate_string('o') == 'o'
