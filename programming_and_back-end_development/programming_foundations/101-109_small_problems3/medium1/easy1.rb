9d803e01fe33c72b4f6991fd7e81c0909ecb819a
Robin Wagner <robin-wagner@web.de>
2017-04-14 02:07:31 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy1.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy1.rb
@@ -6,6 +6,11 @@ def rotate_array(array)
   new_array << array[0]
 end
 
+# Alternative solution
+def rotate_array(array)
+  array[1..-1] + [array[0]]
+end
+
 # Test cases
 p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
 p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
