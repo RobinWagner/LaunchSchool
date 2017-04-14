ca2b0bd19af263220b8a6caf6e9b6031dcb16f55
Robin Wagner <robin-wagner@web.de>
2017-04-14 02:05:48 +0200

    Exercise 1 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems3/medium1/easy1.rb
@@ -0,0 +1,16 @@
+def rotate_array(array)
+  new_array = []
+  1.upto(array.size - 1) do |element|
+    new_array << array[element]
+  end
+  new_array << array[0]
+end
+
+# Test cases
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+p rotate_array(x) == [2, 3, 4, 1]   # => true
+p x == [1, 2, 3, 4]                 # => true
