e884d4afeb9174cc0367c4d0a5df9d2df9d701ed
Robin Wagner <robin-wagner@web.de>
2017-04-16 21:14:37 +0200

    Add alternative solutino

--- programming_and_back-end_development/programming_foundations/AppAcademyProblems/exercise3.rb
+++ programming_and_back-end_development/programming_foundations/AppAcademyProblems/exercise3.rb
@@ -6,6 +6,11 @@ def longest_word(sentence)
   result
 end
 
+# Alternative solution
+def longest_word(sentence)
+  sentence.split.sort_by { |word| word.size }[-1]
+end
+
 # Test cases
 p longest_word('short longest') == 'longest'
 p longest_word('one') == 'one'
