d035474998c66276d8f2d05d4ae4f5136ead58a7
Robin Wagner <robin-wagner@web.de>
2017-04-17 21:22:20 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/AppAcademyProblems/exercise9.rb
+++ programming_and_back-end_development/programming_foundations/AppAcademyProblems/exercise9.rb
@@ -7,6 +7,16 @@ def two_sum(nums)
   nil
 end
 
+# Alternative solution
+def two_sum(nums)
+  nums.each_with_index do |n1, idx1|
+    nums.each_with_index do |n2, idx2|
+      return [idx1, idx2] if n1 + n2 == 0
+    end
+  end
+  nil
+end
+
 # Test cases
 p two_sum([1, 3, 5, -3]) == [1, 3]
 p two_sum([1, 3, 5]) == nil
