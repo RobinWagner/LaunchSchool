3f3368d4baab0f287b8a5bf9e55a3c734b1fee4c
Robin Wagner <robin-wagner@web.de>
2017-04-18 20:05:38 +0200

    Add alternative solution

--- programming_and_back-end_development/programming_foundations/AppAcademyProblems/exercise14.rb
+++ programming_and_back-end_development/programming_foundations/AppAcademyProblems/exercise14.rb
@@ -2,6 +2,11 @@ def capitalize_words(string)
   string.split.map { |word| word.capitalize }.join(' ')
 end
 
+# Alternative solution
+def capitalize_words(string)
+  string.split.map { |word| word[0].upcase + word[1..-1] }.join(' ')
+end
+
 # Test cases
 p capitalize_words("this is a sentence") == "This Is A Sentence"
 p capitalize_words("mike bloomfield") == "Mike Bloomfield"
