93dcc2c91e019fe2163bc7980f56ef80aa64afb8
Robin Wagner <robin-wagner@web.de>
2017-04-22 06:07:43 +0200

    Small correction

--- programming_and_back-end_development/programming_foundations/101-109_small_problems4/easy2/easy3.rb
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems4/easy2/easy3.rb
@@ -4,8 +4,8 @@ bill = gets.chomp.to_f
 print "What is the tip percentage? "
 tip_percentage = gets.chomp.to_f
 
-tip =  (bill * (tip_percentage / 100)).round(2)
-total = bill + tip
+tip   = (bill * (tip_percentage / 100)).round(2)
+total = (bill + tip).round(2)
 
 puts "The tip is $#{tip}"
-puts "The total is $#{total}" 
+puts "The total is $#{total}"
