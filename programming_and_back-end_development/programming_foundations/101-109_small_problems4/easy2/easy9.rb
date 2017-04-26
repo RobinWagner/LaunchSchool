805780c117068f7c73531132f28e38c4cd07ff06
Robin Wagner <robin-wagner@web.de>
2017-04-26 12:25:40 +0200

    Exercise 9 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems4/easy2/easy9.rb
@@ -0,0 +1,14 @@
+name = 'Bob'
+save_name = name
+name = 'Alice'
+puts name, save_name
+
+# => 'Alice', 'Bob'
+
+name = 'Bob'
+save_name = name
+name.upcase!
+puts name, save_name
+
+# => 'BOB', 'BOB'
+# Destructive method mutates referenced object, not just assignment
