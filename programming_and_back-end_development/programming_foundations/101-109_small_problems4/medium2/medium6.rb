a7081bc7d1ee792f20c4db2ef7dee694ff8ed371
Robin Wagner <robin-wagner@web.de>
2017-05-06 14:42:13 +0200

    Exercise 6 completed

---
+++ programming_and_back-end_development/programming_foundations/101-109_small_problems4/medium2/medium6.rb
@@ -0,0 +1,21 @@
+def triangle(angle1, angle2, angle3)
+  angles = [angle1, angle2, angle3]
+  if angles.reduce(:+) == 180 && angles.min > 0
+    if angles.max == 90
+      :right
+    elsif angles.max < 90
+      :acute
+    else
+      :obtuse
+    end
+  else
+    :invalid
+  end
+end
+
+# Test cases
+p triangle(60, 70, 50) == :acute
+p triangle(30, 90, 60) == :right
+p triangle(120, 50, 10) == :obtuse
+p triangle(0, 90, 90) == :invalid
+p triangle(50, 50, 50) == :invalid
