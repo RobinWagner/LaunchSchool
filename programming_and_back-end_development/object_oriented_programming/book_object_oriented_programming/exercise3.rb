0a6bac584a0f20ea6e10787e56e8f790cf979e67
Robin Wagner <robin-wagner@web.de>
2017-05-13 11:44:23 +0200

    Exercise 3 completed

---
+++ programming_and_back-end_development/object_oriented_programming/book_object_oriented_programming/exercise3.rb
@@ -0,0 +1,42 @@
+# my_car.rb
+
+class MyCar
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+end
+
+lumina = MyCar.new(1997, 'chevy lumnia', 'white')
+lumina.speed_up(20)
+lumina.current_speed
+lumina.speed_up(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.shut_down
+lumina.current_speed
