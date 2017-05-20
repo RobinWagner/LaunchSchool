1648e3caec725738b7fd1d8193b45e119828c2d6
Robin Wagner <robin-wagner@web.de>
2017-05-20 11:37:02 +0200

    Exercise 1 completed

---
+++ programming_and_back-end_development/object_oriented_programming/lesson2/inheritance/exercise1.rb
@@ -0,0 +1,25 @@
+class Dog
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming!'
+  end
+end
+
+class Bulldog < Dog
+  def swim
+    "can't swim!"
+  end
+end
+
+teddy = Dog.new
+puts teddy.speak
+puts teddy.swim
+
+puts '---'
+
+frank = Bulldog.new
+puts frank.speak
+puts frank.swim
